b14990b894d5bb5636b136bb79115acd3ae74b1d
Garegin A. Papoian <gpapoian@umd.edu>
2012-05-12 15:13:36 -0400

    Implemented PIMPL scheme of calling the Chem dynamics methods

--- CytoSim/ChemNRM.cpp
+++ CytoSim/ChemNRM.cpp
@@ -2,10 +2,44 @@
 //  ChemNRM.cpp
 //  CytoSim
 //
-//  Created by Garegin Papoian on 5/2/12.
+//  Created by Garegin Papoian on 5/10/12.
 //  Copyright (c) 2012 University of Maryland. All rights reserved.
 //
 
 #include <iostream>
 #include "ChemNRM.h"
-#include <boost/heap/pairing_heap.hpp>
+#include "ChemNRMImpl.h"
+#include "utility.h"
+
+ChemNRM::ChemNRM()
+{
+    _pimpl=make_unique<ChemNRMImpl>();
+}
+
+void ChemNRM::addReaction(Reaction *r){
+    _pimpl->addReaction(r);
+}
+
+void ChemNRM::removeReaction(Reaction *r){
+    _pimpl->removeReaction(r); 
+}
+
+void ChemNRM::run(int steps){
+    _pimpl->run(steps);
+}
+
+size_t ChemNRM::getSize() const {
+    return _pimpl->getSize();
+}
+
+float ChemNRM::getTime() const {
+    return _pimpl->getTime();
+}
+
+void ChemNRM::initialize() {
+    return _pimpl->initialize();
+}
+
+void ChemNRM::printReactions() const {
+    return _pimpl->printReactions();
+}
