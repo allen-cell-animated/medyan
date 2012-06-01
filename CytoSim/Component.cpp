a5d902eb36350080792aa6003b74bf64d2a8b2a5
Garegin A. Papoian <gpapoian@umd.edu>
2012-06-01 10:42:27 -0400

    added doxygen; working on composites

---
+++ CytoSim/Component.cpp
@@ -0,0 +1,9 @@
+//
+//  Component.cpp
+//  CytoSim
+//
+//  Created by Garegin Papoian on 5/31/12.
+//  Copyright (c) 2012 University of Maryland. All rights reserved.
+//
+
+#include <iostream>
---
+++ CytoSim/Component.h
@@ -0,0 +1,14 @@
+//
+//  Component.h
+//  CytoSim
+//
+//  Created by Garegin Papoian on 5/31/12.
+//  Copyright (c) 2012 University of Maryland. All rights reserved.
+//
+
+#ifndef CytoSim_Component_h
+#define CytoSim_Component_h
+
+
+
+#endif
