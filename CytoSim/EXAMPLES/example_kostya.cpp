e4482989618e07c412c6e2e399f8ff280f0bc75e
Garegin A. Papoian <gpapoian@umd.edu>
2012-06-01 21:17:11 -0400

    Added example_kostya

---
+++ CytoSim/EXAMPLES/example_kostya.cpp
@@ -0,0 +1,23 @@
+//
+//  example_kostya.cpp
+//  CytoSim
+//
+//  Created by Garegin Papoian on 6/1/12.
+//  Copyright (c) 2012 University of Maryland. All rights reserved.
+//
+
+#include <iostream>
+
+#include "Species.h"
+
+using namespace std;
+using namespace chem;
+
+int main(int argc, const char * argv[])
+{
+    
+    SpeciesBulk A1("A1",  25);
+    cout << A1 << endl;
+    
+    return 0;
+}
