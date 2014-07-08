3b6c5e23e52550b99694f32631272e4a81460351
James Eric Komianos <jkomianos@gmail.com>
2014-07-08 09:49:43 -0400

    7/8/14 - getting reactioncontainer from other branch. no other changes

--- CytoSim/ReactionContainer.h
+++ CytoSim/ReactionContainer.h
@@ -14,26 +14,41 @@
 #include "Reaction.h"
 
 namespace  chem {
-        
+    
+    ///An abstract interface for a container of pointers to reaction objects
     class ReactionPtrContainerIFace {
     public:
+        ///Clear the reaction container
         virtual void clear() = 0;
+        
+        ///Add a unique reaction pointer to this container
         virtual ReactionBase* addReactionUnique(std::unique_ptr<ReactionBase> &&Reaction) = 0;
+        
+        ///Remove a reaction from this container
         virtual void removeReaction(ReactionBase* Reaction) = 0;
+        
+        ///Find a reaction in this container
         virtual ReactionBase* findReaction (size_t index) = 0;
+        
+        ///Print all reactions from this container
         virtual void printReaction() {}
     };
-    
-    
-    
-    
+
+    /// A concrete class implementing the ReactionPtrContainerIFace,
+    /// using std::vector<std::unique_ptr<ReactionBase>> as the container implementation
     class ReactionPtrContainerVector : public  ReactionPtrContainerIFace {
     protected:
-        std::vector<std::unique_ptr<ReactionBase>> _reactions;
+        std::vector<std::unique_ptr<ReactionBase>> _reactions;  ///Reaction ptr container
     public:
+        
+        ///Default constructor
         ReactionPtrContainerVector() : _reactions() {}
+        
+        ///Copying not allowed
         ReactionPtrContainerVector(const ReactionPtrContainerVector &) = delete;
-        ReactionPtrContainerVector& operator=(ReactionPtrContainerVector &) = delete;  // no assignment
+        
+        ///Assignment not allowed
+        ReactionPtrContainerVector& operator=(ReactionPtrContainerVector &) = delete;
 
         friend void swap(ReactionPtrContainerVector& first, ReactionPtrContainerVector& second) // nothrow
         {
@@ -42,14 +57,16 @@ namespace  chem {
             swap(first._reactions, second._reactions);
         }
 
+        ///Clear the container
         virtual void clear() {_reactions.clear();}
 
-        
+        ///Add a unique reaction ptr to this container
         virtual ReactionBase* addReactionUnique (std::unique_ptr<ReactionBase> &&Reaction) {
             _reactions.push_back(std::move(Reaction));
             return _reactions.back().get();
         }
         
+        ///Add a general reaction to this container
         template<unsigned short M, unsigned short N, typename ...Args>
         ReactionBase* addReaction( Args&& ...args )
         {
@@ -58,6 +75,56 @@ namespace  chem {
             return _reactions.back().get();
         }
         
+        ///Add a ReactionPolymerizationCapping to this reaction container
+        template<typename ...Args>
+        ReactionBase* addPolymerizationCappingReaction(Args&& ...args)
+        {
+            _reactions.push_back(std::unique_ptr<ReactionBase>( new ReactionPolymerizationCapping( std::forward<Args>(args)...) ));
+            return _reactions.back().get();
+        }
+        
+        ///Add a ReactionDepolymerizationCapping to this reaction container
+        template<typename ...Args>
+        ReactionBase* addDepolymerizationCappingReaction(Args&& ...args)
+        {
+            _reactions.push_back(std::unique_ptr<ReactionBase>( new ReactionDepolymerizationCapping( std::forward<Args>(args)...) ));
+            return _reactions.back().get();
+        }
+        
+        ///Add a ReactionPolymerization to this reaction container
+        template<typename ...Args>
+        ReactionBase* addPolymerizationReaction(Args&& ...args)
+        {
+            _reactions.push_back(std::unique_ptr<ReactionBase>( new ReactionPolymerization( std::forward<Args>(args)...) ));
+            return _reactions.back().get();
+        }
+        
+        ///Add a ReactionDepolymerization to this reaction container
+        template<typename ...Args>
+        ReactionBase* addDepolymerizationReaction(Args&& ...args)
+        {
+            _reactions.push_back(std::unique_ptr<ReactionBase>( new ReactionDepolymerization( std::forward<Args>(args)...) ));
+            return _reactions.back().get();
+        }
+        
+//        ///Add a ReactionBinding to this reaction container
+//        template<typename ...Args>
+//        ReactionBase* addBindingReaction(Args&& ...args)
+//        {
+//            _reactions.push_back(std::unique_ptr<ReactionBase>( new ReactionBinding( std::forward<Args>(args)...) ));
+//            return _reactions.back().get();
+//        }
+//        
+//        ///Add a ReactionUnbinding to this reaction container
+//        template<typename ...Args>
+//        ReactionBase* addUnbindingReaction(Args&& ...args)
+//        {
+//            _reactions.push_back(std::unique_ptr<ReactionBase>( new ReactionUnbinding( std::forward<Args>(args)...) ));
+//            return _reactions.back().get();
+//        }
+        
+        
+        ///Add a general reaction class to this container
         template<template <unsigned short M, unsigned short N> class RXN, unsigned short M, unsigned short N>
         ReactionBase* add(std::initializer_list<Species*> species, float rate)
         {
@@ -66,6 +133,7 @@ namespace  chem {
             return _reactions.back().get();
         }
         
+        ///Remove a reaction from this container
         virtual void removeReaction (ReactionBase* R) {
             auto child_iter = std::find_if(_reactions.begin(),_reactions.end(),
                                            [R](const std::unique_ptr<ReactionBase> &element)
@@ -76,6 +144,7 @@ namespace  chem {
                 _reactions.erase(child_iter);
         }
         
+        ///Remove all reactions that contain a certain species from this container
         virtual void removeReactions (Species* s)
         {
             for(auto &r : _reactions)
@@ -87,20 +156,24 @@ namespace  chem {
             }
         }
         
-        
+        ///Find a reaction by index in this container
+        ///@note no check on the index
         virtual ReactionBase* findReaction (size_t index) {
             return _reactions[index].get();
         }
         
-        
+        ///Get all reactions in vector form
         std::vector<std::unique_ptr<ReactionBase>>& reactions() {return _reactions;}
         const std::vector<std::unique_ptr<ReactionBase>>& reactions() const {return _reactions;}
         
+        ///Print all reactions in this container
         virtual void printReactions() {
             for(auto &r : _reactions)
                 std::cout << (*r.get());
         }
         
+        ///Find a similar reaction in this container (satistifes equality operator)
+        ///@note returns the first similar reaction found
         virtual ReactionBase* findSimilarReaction (const ReactionBase &r) {
             auto it = std::find_if(_reactions.begin(),_reactions.end(),
                                    [&r](const std::unique_ptr<ReactionBase> &element)
