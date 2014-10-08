95838c4545f1f9e7c399782704ee62b9400057e7
James Eric Komianos <jkomianos@gmail.com>
2014-10-08 14:03:39 -0400

    10/8/14 - Restructuring of CCylinder reaction containers.

--- Cyto/Structure/CCylinder.cpp
+++ Cyto/Structure/CCylinder.cpp
@@ -16,13 +16,18 @@ CCylinder::CCylinder(const CCylinder& rhs, Compartment* c) : _compartment(c)
         _monomers.push_back(std::unique_ptr<CMonomer>(m->clone(c)));
     
     ///copy all reactions
-    for(auto &r: rhs._reactions)
-        addReaction(r->clone(c->speciesContainer()));
-    for(auto &r: rhs._frontReactions)
-        addFrontReaction(r->clone(c->speciesContainer()), true);
-    for(auto &r: rhs._backReactions)
-        addBackReaction(r->clone(c->speciesContainer()), true);
+    for(auto &r: rhs._internalReactions)
+        addInternalReaction(r->clone(c->speciesContainer()));
     
+    for(auto it = rhs._crossCylinderReactions.begin(); it != rhs._crossCylinderReactions.end(); it++) {
+        ///Copy map
+        for(auto &r : it->second) addCrossCylinderReaction(it->first, r->clone(c->speciesContainer()));
+    }
+    ///Copy reacting cylinders
+    for(auto &c : rhs._reactingCylinders) addReactingCylinder(c);
+    
+    
+
     ///Update and return
     this->updateReactions();
 }
@@ -30,84 +35,117 @@ CCylinder::CCylinder(const CCylinder& rhs, Compartment* c) : _compartment(c)
 CCylinder::~CCylinder()
 {
     ///Remove all reactions
-    for(auto &r: _reactions)
-        removeReaction(r);
-    for(auto &r: _frontReactions)
-        removeReaction(r);
-    for(auto &r: _backReactions)
-        removeReaction(r);
+    for(auto &r: _internalReactions)
+        removeInternalReaction(r);
+    
+    
     
     ///Remove all species
     for(auto &m: _monomers) {
-        for(auto &s : m->speciesFilamentVector())
-            _compartment->removeSpecies(s);
-        for(auto &s : m->speciesBoundVector())
-            _compartment->removeSpecies(s);
-        for(auto &s : m->speciesPlusEndVector())
-            _compartment->removeSpecies(s);
-        for(auto &s : m->speciesMinusEndVector())
-            _compartment->removeSpecies(s);
+        for(auto &s : m->speciesFilamentVector()) _compartment->removeSpecies(s);
+        for(auto &s : m->speciesBoundVector())    _compartment->removeSpecies(s);
+        for(auto &s : m->speciesPlusEndVector())  _compartment->removeSpecies(s);
+        for(auto &s : m->speciesMinusEndVector()) _compartment->removeSpecies(s);
     }
 }
 
 
-void CCylinder::addReaction(ReactionBase* r) {
+void CCylinder::addInternalReaction(ReactionBase* r) {
     //remove from compartment and chemsim
     _compartment->addInternalReactionUnique(std::unique_ptr<ReactionBase>(r));
     ChemSim::addReaction(ChemSimReactionKey(), r);
     
     ///add to local reaction list
-    _reactions.push_back(r);
+    _internalReactions.push_back(r);
 }
 
-void CCylinder::addFrontReaction(ReactionBase* r, bool manage) {
-    //remove from compartment and chemsim
-    if(manage) {
-        _compartment->addInternalReactionUnique(std::unique_ptr<ReactionBase>(r));
-        ChemSim::addReaction(ChemSimReactionKey(), r);
-    }
+void CCylinder::addCrossCylinderReaction(CCylinder* other, ReactionBase* r) {
+    //add to compartment and chemsim
+    _compartment->addInternalReactionUnique(std::unique_ptr<ReactionBase>(r));
+    ChemSim::addReaction(ChemSimReactionKey(), r);
     
-    ///add to local reaction list
-    _frontReactions.push_back(r);
+    ///add to this reaction map
+    _crossCylinderReactions[other].push_back(r);
+    ///add to others reacting cylinders list
+    other->addReactingCylinder(this);
 }
 
-void CCylinder::addBackReaction(ReactionBase* r, bool manage) {
-    //remove from compartment and chemsim
-    if(manage) {
-        _compartment->addInternalReactionUnique(std::unique_ptr<ReactionBase>(r));
-        ChemSim::addReaction(ChemSimReactionKey(), r);
-    }
-    
-    ///add to local reaction list
-    _backReactions.push_back(r);
-}
+void CCylinder::addReactingCylinder(CCylinder* other) { _reactingCylinders.push_back(other);}
 
-void CCylinder::removeReaction(ReactionBase* r) {
+
+void CCylinder::removeInternalReaction(ReactionBase* r) {
     ///remove from compartment and chemsim
     _compartment->removeInternalReaction(r);
     ChemSim::removeReaction(ChemSimReactionKey(), r);
+    
+    ///remove from internal reaction list
+    auto it = std::find(_internalReactions.begin(), _internalReactions.end(), r);
+    if (it != _internalReactions.end()) _internalReactions.erase(it);
 }
+
+void CCylinder:: removeAllInternalReactions() {
+    
+    for (auto &r : _internalReactions) {
+        ///remove from compartment and chemsim
+        _compartment->removeInternalReaction(r);
+        ChemSim::removeReaction(ChemSimReactionKey(), r);
+    }
+    _internalReactions.clear();
+}
+
+void CCylinder::removeCrossCylinderReactions(CCylinder* other) {
+    
+    ///Remove from this map
+    for(auto &r : _crossCylinderReactions[other]) {
+        _compartment->removeInternalReaction(r);
+        ChemSim::removeReaction(ChemSimReactionKey(), r);
+    }
+    _crossCylinderReactions.erase(other);
+    
+    ///also remove from reacting list of other ccylinder
+    other->removeReactingCylinder(this);
+}
+
+void CCylinder::removeAllCrossCylinderReactions() {
+    
+    for(auto it = _crossCylinderReactions.begin(); it != _crossCylinderReactions.end(); it++) {
+        
+        ///Remove from this map
+        for(auto &r : it->second) {
+            _compartment->removeInternalReaction(r);
+            ChemSim::removeReaction(ChemSimReactionKey(), r);
+        }
+        
+        ///also remove from map of other ccylinder
+        it->first->removeReactingCylinder(this);
+    }
+    _crossCylinderReactions.clear();
+}
+
+void CCylinder::removeReactingCylinder(CCylinder* other) {
+    auto it = std::find(_reactingCylinders.begin(), _reactingCylinders.end(), other);
+    if (it != _reactingCylinders.end()) _reactingCylinders.erase(it);
+}
+
 
 void CCylinder::updateReactions()
 {
     ///loop through all reactions, passivate/activate
-    for(auto &r : _backReactions) {
+    for(auto &r : _internalReactions) {
         if(r->getProductOfReactants() == 0)
             r->passivateReaction();
         else
             r->activateReaction();
     }
-    for(auto &r : _reactions) {
-        if(r->getProductOfReactants() == 0)
-            r->passivateReaction();
-        else
-            r->activateReaction();
-    }
-    for(auto &r : _frontReactions) {
-        if(r->getProductOfReactants() == 0)
-            r->passivateReaction();
-        else
-            r->activateReaction();
+    
+    for(auto it = _crossCylinderReactions.begin(); it != _crossCylinderReactions.end(); it++) {
+        
+        for(auto &r : it->second) {
+            if(r->getProductOfReactants() == 0)
+                r->passivateReaction();
+            else
+                r->activateReaction();
+        }
     }
 }
 
--- Cyto/Structure/CCylinder.h
+++ Cyto/Structure/CCylinder.h
@@ -19,11 +19,19 @@
 class ChemSimReactionKey;
 class Cylinder;
 
-/// CCylinder class holds all CMonomers
+/// CCylinder class holds all CMonomers and reactions associated with its species
 /*! 
  *  The CCylinder class has lists of the monomers and bounds that it contains.
- *  it has functionality to print the current composition.
- *  Accessing a particular species in the CCylinder is possible as well.
+ *
+ *  A CCylinder also stores the reaction associated with it internally, as well as cross-cylinder
+ *  reactions (still "owned" by this ccylinder, but has reactants and/or products that contain a
+ *  species in another ccylinder). This information is stored in the reaction map. This map can be
+ *  used to add and delete reactions when a ccylinder changes compartment or is removed from the
+ *  system.
+ *
+ *  Lastly, the CCylinder stores a list of other ccylinders that it has cross-cylinder reactions with,
+ *  but does not have ownership of these reactions. This list can be used to change or delete reactions
+ *  when needed, but will not be tied to a compartment transfer of this ccylinder.
  */
 class CCylinder {
     
@@ -31,13 +39,13 @@ protected:
     std::vector<std::unique_ptr<CMonomer>> _monomers; ///< list of monomers in this ccylinder
     
     ///REACTION CONTAINERS
-    std::vector<ReactionBase*> _reactions;///< list of reactions associated with ccylinder
-    std::vector<ReactionBase*> _frontReactions; ///< list of reactions involving the next ccylinder
-    std::vector<ReactionBase*> _backReactions; ///< list of reactions involving the previous ccylinder
+    std::vector<ReactionBase*> _internalReactions;///< list of internal reactions associated with ccylinder
+    std::map<CCylinder*, std::vector<ReactionBase*>> _crossCylinderReactions; ///< map of cross-cylinder reactions owned by this ccylinder
+    std::vector<CCylinder*> _reactingCylinders; ///< vector of ccylinders that this ccylinder has reactions with, but not ownership
     
     Compartment* _compartment; ///< compartment this ccylinder is in
 
-    Cylinder* _pCylinder;
+    Cylinder* _pCylinder; ///< parent cylinder
     
     short _size = SystemParameters::Geometry().cylinderSize / SystemParameters::Geometry().monomerSize; ///<max length of full cylinder
 
@@ -76,38 +84,39 @@ public:
     ///@note no check on index
     CMonomer* getCMonomer(int index) {return _monomers[index].get();}
     
-    ///Add a reaction to this CCylinder
-    void addReaction(ReactionBase* r);
-    ///Add a reaction with the front ccylinderto this CCylinder
+    ///Add an internal reaction to this CCylinder
+    void addInternalReaction(ReactionBase* r);
+    ///Add a reaction across this ccylinder and another ccylinder
+    ///Adds to this reaction map as well as the other ccylinder's reaction map
     ///@note manage decides whether this reaction adding will also
     ///add to the chemsim and compartment.
-    void addFrontReaction(ReactionBase* r, bool manage=false);
-    ///Add a reaction with the back ccylinderto this CCylinder
-    ///@note manage decides whether this reaction adding will also
-    ///add to the chemsim and compartment.
-    void addBackReaction(ReactionBase* r, bool manage=false);
+    void addCrossCylinderReaction(CCylinder* other, ReactionBase* r);
+    ///Add a reacting ccylinder to this one
+    void addReactingCylinder(CCylinder* other);
     
-    ///remove a filament reaction from this system
+    ///remove an internal reaction from this ccylinder
     ///@note no check on whether r is in the reactions list
-    void removeReaction(ReactionBase* r);
+    void removeInternalReaction(ReactionBase* r);
+    ///remove all internal reactions from this ccylinder
+    void removeAllInternalReactions();
     
-    ///clear all filament reactions from front
-    ///typically called when depolymerizing
-    void clearFrontReactions() {_frontReactions.clear();}
-    ///clear all filament reactions from back
-    ///typically called when depolymerizing
-    void clearBackReactions() {_backReactions.clear();}
+    ///clear all reactions involving another ccylinder
+    ///@note also removes reaction from other ccylinder's map
+    void removeCrossCylinderReactions(CCylinder* other);
+    ///clear all reactions involving other cylinders
+    ///@note also removes all reactions from other ccylinder's map
+    void removeAllCrossCylinderReactions();
+    
+    ///remove a reacting ccylinder from this one
+    void removeReactingCylinder(CCylinder* other);
     
     ///Get list of reactions associated with this CCylinder
-    const std::vector<ReactionBase*>& getReactions() {return _reactions;}
-    ///Get list of reactions associated with this CCylinder and next
-    const std::vector<ReactionBase*>& getFrontReactions() {return _frontReactions;}
-    ///Get list of reactions associated with this CCylinder and previous
-    const std::vector<ReactionBase*>& getBackReactions() {return _backReactions;}
+    const std::vector<ReactionBase*>& getInternalReactions() {return _internalReactions;}
+    ///Get map of reactions associated with this CCylinder and others
+    std::map<CCylinder*, std::vector<ReactionBase*>>& getCrossCylinderReactions() {return _crossCylinderReactions;}
     
     ///Update all reactions associated with this CCylinder
     void updateReactions();
-    
     ///Print CCylinder
     virtual void printCCylinder();
     
