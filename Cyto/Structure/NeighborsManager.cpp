8c4f66a5e4cb811c5c58d7553ffc5df8c08fd922
James Eric Komianos <jkomianos@gmail.com>
2014-11-10 15:51:18 -0500

    11/10/14 - Added neighbors list class

---
+++ Cyto/Structure/NeighborsManager.cpp
@@ -0,0 +1,9 @@
+//
+//  NeighborsManager.cpp
+//  Cyto
+//
+//  Created by James Komianos on 11/10/14.
+//  Copyright (c) 2014 University of Maryland. All rights reserved.
+//
+
+#include "NeighborsManager.h"
---
+++ Cyto/Structure/NeighborsManager.h
@@ -0,0 +1,48 @@
+//
+//  NeighborsManager.h
+//  Cyto
+//
+//  Created by James Komianos on 11/10/14.
+//  Copyright (c) 2014 University of Maryland. All rights reserved.
+//
+
+#ifndef __Cyto__NeighborsManager__
+#define __Cyto__NeighborsManager__
+
+#include <stdio.h>
+#include <boost/graph/adjacency_list.hpp>
+
+using namespace boost;
+
+typedef adjacency_list<listS, hash_setS, undirectedS> NeighborList;
+
+
+class NeighborsManager {
+    
+private:
+    
+    
+    
+    
+    
+    
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* defined(__Cyto__NeighborsManager__) */
