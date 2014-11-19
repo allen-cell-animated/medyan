e668fa3e942a79388636a2ccd2e3655483cc35f5
James Eric Komianos <jkomianos@gmail.com>
2014-11-19 15:37:09 -0500

    11/19/14 - Changed back edit to CGMinimization

--- Cyto/Mechanics/Minimizer/CGMethod.cpp
+++ Cyto/Mechanics/Minimizer/CGMethod.cpp
@@ -303,7 +303,7 @@ double CGMethod::backtrackingLineSearch(ForceFieldManager& FFM) {
         
         if(lambda <= 0.0 || idealEnergyChange >= -LSENERGYTOL) {
             
-            if(energyChange < -LSENERGYTOL) {
+            if(energyChange < 0) {
                 _energyChangeCounter = 0;
                 return lambda;
             }
@@ -380,7 +380,7 @@ double CGMethod::quadraticLineSearch(ForceFieldManager& FFM) {
         
         if(lambda <= 0.0 || idealEnergyChange >= -LSENERGYTOL) {
             
-            if(energyChange < -LSENERGYTOL) {
+            if(energyChange < 0) {
                 _energyChangeCounter = 0;
                 return lambda;
             }
