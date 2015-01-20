2cc2f405db8fc6873eda8d12b363257128896404
James Eric Komianos <jkomianos@gmail.com>
2015-01-20 15:35:44 -0500

    1/20/15 - Took out some print statements

--- M3SYM/Chemistry/ReactionManager.h
+++ M3SYM/Chemistry/ReactionManager.h
@@ -77,8 +77,6 @@ public:
         
         //add all other binding sites
         while(bindingCount < SystemParameters::Geometry().cylinderIntSize) {
-            
-            cout << "BINDING SITE = " << bindingCount << endl;
             _bindingSites.push_back(bindingCount);
             bindingCount += deltaBinding;
         }
