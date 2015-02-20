9468372384728c12f2f14b411884f095b3069b29
James Eric Komianos <jkomianos@gmail.com>
2015-02-20 13:38:23 -0500

    2/20/15 - Created dynamic rates branch

--- M3SYM/RateChanger.cpp
+++ M3SYM/RateChanger.cpp
@@ -18,18 +18,27 @@ float BrownianRatchet::changeRate(float bareRate, double force) {
     return bareRate * exp( - force * _x / kT);
 }
 
-float CatchSlipBond::changeRate(float bareRate, double force) {
+float LinkerCatchSlip::changeRate(float bareRate, double force) {
 
     return bareRate * (_a1 * exp(- force * _x1 / kT) +
-                       _a2 * exp( force * _x2 / kT));
+                       _a2 * exp(  force * _x2 / kT));
 }
 
-float SlipBond::changeRate(float bareRate, double force) {
+float LinkerSlip::changeRate(float bareRate, double force) {
     
     return bareRate * exp( force * _x / kT);
 }
 
-float ExpStall::changeRate(float bareRate, double force) {
+float MotorPCMCatch::changeRate(float bareRate, double force) {
+    
+    //first determine n_b
+    
+    
+    
+    
+}
+
+float MotorHillStall::changeRate(float bareRate, double force) {
     
     return bareRate * exp( - force * _x / kT);
 }
--- M3SYM/RateChanger.h
+++ M3SYM/RateChanger.h
@@ -31,7 +31,7 @@ public:
     virtual float changeRate(float bareRate, double force) = 0;
 };
 
-/// A brownian ratchet implementation of the DynamicRateChanger.
+/// A brownian ratchet implementation of the RateChanger.
 /// Used for filament polymerization when under load force.
 
 /// @note - This function updates polymerization rates based on the
@@ -49,8 +49,8 @@ public:
     virtual float changeRate(float bareRate, double force);
 };
 
-///A catch-slip bond implementation of the DynamicRateChanger.
-///Used for either cross-linker or motor unbinding when under stress.
+///A catch-slip bond implementation of the RateChanger.
+///Used for cross-linker unbinding when under stress.
 
 /// @note - This function updates unbinding rates based on the
 /// following exponential form (Guo et al 2006):
@@ -60,7 +60,7 @@ public:
 ///  where x and a are the characteristic lengths and amplitudes
 ///  of the catch and slip portions of the function, respectively.
 
-class CatchSlipBond : public RateChanger {
+class LinkerCatchSlip : public RateChanger {
     
 private:
     double _a1; ///< catch bond amplitude
@@ -69,16 +69,16 @@ private:
     double _x2; ///< slip bond characteristic length
     
 public:
-    CatchSlipBond(double amplitude1, double amplitude2,
-                  double charLength1, double charLength2) :
+    LinkerCatchSlip(double amplitude1, double amplitude2,
+                    double charLength1, double charLength2) :
         _a1(amplitude1),  _a2(amplitude2),
         _x1(charLength1), _x2(charLength2) {}
     
     virtual float changeRate(float bareRate, double force);
 };
 
-///A slip bond implementation of the DynamicRateChanger.
-///Used for either cross-linker or motor unbinding when under stress.
+///A slip bond implementation of the RateChanger.
+///Used for cross-linker unbinding when under stress.
 
 /// @note - This function updates unbinding rates based on the
 /// following exponential form (Bell form, Bell et al, 1978):
@@ -87,35 +87,71 @@ public:
 ///
 /// So as to exponetially increase the unbinding with more force.
 
-class SlipBond : public RateChanger {
+class LinkerSlip : public RateChanger {
     
 private:
     double _x; ///< The characteristic length for this function
     
 public:
-    SlipBond(double charLength) : _x(charLength) {}
+    LinkerSlip(double charLength) : _x(charLength) {}
     
     virtual float changeRate(float bareRate, double force);
 };
 
-///A stall force implementation of the DynamicRateChanger.
+///A PCM catch bond implementation of the RateChanger
+///Used for motor unbinding when under stress
+
+/// @note - This function updates unbinding rates based on the
+/// following exponential form (Erdmann et al, JACS 2013):
+///
+///      k_eff = N_b * k_unbinding * exp(-F / (N_b * F_0))
+///
+/// where F_0 is the characteristic force defining this catch,
+/// and N_b is the number of bound motor heads determined by
+///
+///             N_b = 0.33*N_t + (F * alpha)
+///
+/// where alpha has been empirically determined to be 0.028
+class MotorPCMCatch : public RateChanger {
+    
+private:
+    
+    double _F0;  ///< characteristic force
+    int _Nt; ///< number of motor heads
+    
+public:
+    MotorPCMCatch(double charForce, int numHeads)
+        : _F0(charForce), _Nt(numHeads) {}
+    
+    virtual float changeRate(float bareRate, double force);
+};
+
+
+///A Hill form stall force implementation of the RateChanger.
 ///Used for motor walking when under stress.
 
 /// @note - This function updates walking rates based on the
-/// following exponential form:
+/// following form (based on Erdmann et al, JACS 2013):
 ///
-///                 k = k_0 * exp(-f * x / kT)
+///   k_eff = (k_w / N_b) * (F_0 - F) / (F_0 + (F / beta))
 ///
-/// so as to exponentially decrease the walking rate with
-/// more force.
+/// where F_0 is the characteristic force defining this catch,
+/// beta has been empirically determined to be 0.21, and
+/// N_b is the number of bound motor heads determined by
+///
+///             N_b = 0.33*N_t + (F * alpha)
+///
+/// where alpha has been empirically determined to be 0.028
 
-class ExpStall : public RateChanger  {
+class MotorHillStall : public RateChanger  {
     
 private:
-    double _x; ///< The characteristic length for this function
+    double _F0;  ///< characteristic force
+    int _Nt; ///< number of motor heads
     
 public:
-    ExpStall(double charLength) : _x(charLength) {}
+    MotorHillStall(double charForce, int numHeads)
+        : _F0(charForce), _Nt(numHeads) {}
     
     virtual float changeRate(float bareRate, double force);
 };
