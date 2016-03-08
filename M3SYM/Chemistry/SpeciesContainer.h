5140898b9f2c7cf98177344d13231f2eb36489e3
James Eric Komianos <jkomianos@gmail.com>
2016-03-08 14:50:42 -0500

    3/8/16 - Removing some print statements.

--- M3SYM/Chemistry/SpeciesContainer.h
+++ M3SYM/Chemistry/SpeciesContainer.h
@@ -144,8 +144,7 @@ public:
         auto child_iter = find_if(_species.begin(),_species.end(),
           [&name](const unique_ptr<Species> &element) {
           return element->getName()==name ? true : false;});
-        if(child_iter!=_species.end()) {
-            cout << "We found it!" << endl;
+        if(child_iter!=_species.end()) {s
             return child_iter->get();
         }
         
