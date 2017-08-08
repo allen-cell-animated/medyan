4c15cec9d4251184a3b191fd4ec15d239da806d6
Yossi Eliaz <eliaz123@gmail.com>
2017-08-08 09:51:02 -0500

    Camkii: Added place holders for FF
    Added Camkii bending and stretching
    class + cosine + harmonic descendants
    
    Signed-off-by: Yossi Eliaz <eliaz123@gmail.com>

---
+++ src/Mechanics/ForceField/Camkii/CamkiiStretching.h
@@ -0,0 +1,37 @@
+
+//------------------------------------------------------------------
+//  **MEDYAN** - Simulation Package for the Mechanochemical
+//               Dynamics of Active Networks, v3.1
+//
+//  Copyright (2015-2016)  Papoian Lab, University of Maryland
+//
+//                 ALL RIGHTS RESERVED
+//
+//  See the MEDYAN web page for more information:
+//  http://www.medyan.org
+//------------------------------------------------------------------
+
+#ifndef MEDYAN_FilamentStretching_h
+#define MEDYAN_FilamentStretching_h
+
+#include "common.h"
+
+#include "FilamentInteractions.h"
+
+/// Represents a Filament stretching interaction
+template <class FStretchingInteractionType>
+class FilamentStretching : public FilamentInteractions {
+    
+private:
+    FStretchingInteractionType _FFType; 
+    
+public:
+    virtual double computeEnergy(double d);
+    virtual void computeForces();
+    virtual void computeForcesAux();
+    
+    virtual const string getName() {return "Filament Stretching";}
+};
+
+
+#endif
