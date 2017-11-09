99218fd9ef5a30a66916e921d65e823249d2dd91
drelatgithub <drel.sohu.com@gmail.com>
2017-11-09 16:58:23 -0500

    Made membrane related to the database

--- src/Structure/Membrane.h
+++ src/Structure/Membrane.h
@@ -18,6 +18,24 @@ class Membrane: public Composite, public Trackable {
 private:
 
     SubSystem* _subSystem; // SubSystem pointer
+
+    static Database<Membrane*> _membranes; // Collection in SubSystem
+
+public:
+    // SubSystem management, inherited from Trackable
+    virtual void addToSubSystem() { _membranes.addElement(this); }
+    virtual void removeFromSubSystem() { _membranes.removeElement(this); }
+    
+    /// Get all instances of this class from the SubSystem
+    static const vector<Membrane*>& getMembranes() {
+        return _membranes.getElements();
+    }
+    /// Get the number of membranes in this system
+    static int numFilaments() {
+        return _membranes.countElements();
+    }
+
+
 };
 
 
