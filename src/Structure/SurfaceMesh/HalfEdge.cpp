5d28b04c31d0acb8e48092185fc7f35fd2d3b145
drelatgithub <drel.sohu.com@gmail.com>
2017-12-01 15:57:13 -0500

    Removed HalfEdge

--- src/Structure/SurfaceMesh/HalfEdge.cpp
+++
@@ -1,8 +0,0 @@
-#include "HalfEdge.h"
-
-HalfEdge::HalfEdge(Composite* parent, Bead* b1, Bead* b2):
-    _b{b1, b2}, _neighborTriangle{nullptr, nullptr} {
-    
-    parent -> addChild(unique_ptr<Component>(this));
-
-}
