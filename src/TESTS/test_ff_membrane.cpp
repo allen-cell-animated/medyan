d36dad4d6b4ec7a245cfb030f7f4e1be4660fada
drelatbitbucket <drel.sohu.com@gmail.com>
2017-12-29 18:52:59 -0500

    Implemented force field test for membrane, but something is wrong with curvature...

--- src/TESTS/test_ff_membrane.cpp
+++ src/TESTS/test_ff_membrane.cpp
@@ -29,12 +29,17 @@ using namespace mathfunc;
 #    include "SubSystem.h"
 
 #    include "Membrane.h"
+#    include "Vertex.h"
+#    include "MVoronoiCell.h"
+#    include "Triangle.h"
+#    include "MTriangle.h"
 
+#    include "MembraneInteractions.h"
 #    include "MembraneStretching.h"
 #    include "MembraneStretchingHarmonic.h"
 #    include "MembraneStretchingVoronoiHarmonic.h"
 #    include "MembraneBending.h"
-#    include "MembraneBendingVoronoi.h"
+#    include "MembraneBendingVoronoiHelfrich.h"
 
 namespace {
     using VertexData = tuple<array<double, 3>, vector<size_t>>;
@@ -81,7 +86,7 @@ namespace {
             SysParams::MParams.MemEqCurv.resize(1, 0);
 
             m = new Membrane(&s, 0, memData);
-            m.addToSubSystem();
+            m->addToSubSystem();
         }
         ~MembraneFFTest() {
             SysParams::GParams.cylinderNumMon.resize(0);
@@ -90,7 +95,7 @@ namespace {
             SysParams::MParams.MemBendingK.resize(0);
             SysParams::MParams.MemEqCurv.resize(0);
 
-            m.removeFromSubSystem();
+            m->removeFromSubSystem();
             delete m;
         }
 
@@ -102,20 +107,31 @@ namespace {
     void resetCoordinate(Membrane *m) {
         for(Vertex* it: m->getVertexVector()) it->coordinate = it->coordinateP;
     }
-    void assignRandomForce(Membrane* m, double sigma) {
-        normal_distribution<> nd(0, sigma);
+    void resetForce(Membrane *m) {
+        for(Vertex* it: m->getVertexVector()) it->force.assign(3, 0);
+    }
+    void assignRandomForceAuxP(Membrane* m, double sigma) {
+		normal_distribution<> nd(0, sigma);
 
+		for(Vertex* it: m->getVertexVector()) {
+			for(double& eachForce: it->forceAuxP) { // forceAuxP is used here simply because it is an empty container.
+				eachForce = nd(Rand::engFixed);
+			}
+		}
+    }
+    void moveAlongForceAuxP(Membrane* m, double d) {
         for(Vertex* it: m->getVertexVector()) {
-            for(double& eachForce: it->force) {
-                eachForce = nd(Rand::engFixed);
+            for(size_t coordIdx = 0; coordIdx < 3; ++coordIdx) {
+                it->coordinate[coordIdx] += it->forceAuxP[coordIdx] * d;
             }
         }
     }
-    void moveAlongForce(Membrane* m, double d) {
-        for(Vertex* it: m->getVertexVector()) {
-            for(size_t coordIdx = 0; coordIdx < 3; ++coordIdx) {
-                it->coordinate[coordIdx] += it->force[coordIdx] * d;
-            }
+    void resizeEqArea(Membrane *m, double ratio) {
+        for(Triangle* t: m->getTriangleVector()) {
+            t->getMTriangle()->setEqArea(t->getMTriangle()->getEqArea() * ratio);
+        }
+        for(Vertex* v: m->getVertexVector()) {
+            v->getMVoronoiCell()->setEqArea(v->getMVoronoiCell()->getEqArea() * ratio);
         }
     }
 }
@@ -126,218 +142,54 @@ TEST_F(MembraneFFTest, CompareStretchingEnergy) {
     MembraneStretching<MembraneStretchingHarmonic> mSTriangle;
     MembraneStretching<MembraneStretchingVoronoiHarmonic> mSVoronoi;
 
-    EXPECT_DOUBLE_EQ(mSTriangle.computeEnergy(0.0), mSVoronoi.computeEnergy(0.0));
-
-}
-
-TEST_F(MembraneFFTest, Geometry) {
-
-    /**************************************************************************
-        Check normal geometry
-    **************************************************************************/
-    m->updateGeometry(true);
-
-    // Check edge length
-    double exEdgeLen = radius * sqrt(2);
-    for(Edge* it: m->getEdgeVector())
-        EXPECT_DOUBLE_EQ(it->getMEdge()->getLength(), exEdgeLen);
-    // Check triangle area and angle
-    double exTriangleArea = radius * radius * sqrt(3) / 2;
-    double exTriangleAngle = M_PI / 3;
-    for(Triangle* it: m->getTriangleVector()) {
-        EXPECT_DOUBLE_EQ(it->getMTriangle()->getArea(), exTriangleArea);
-        for(double eachTheta: it->getMTriangle()->getTheta())
-            EXPECT_DOUBLE_EQ(eachTheta, exTriangleAngle);
-    }
-    // Check Voronoi cell area and curvature
-    double exVCellArea = radius * radius * sqrt(3) * 2 / 3;
-    double exVCellCurv = 1 / radius;
-    for(Vertex* it: m->getVertexVector()) {
-        EXPECT_DOUBLE_EQ(it->getMVoronoiCell()->getArea(), exVCellArea);
-        EXPECT_DOUBLE_EQ(it->getMVoronoiCell()->getCurv(), exVCellCurv);
-    }
-
-    /**************************************************************************
-        Check stretched geometry
-    **************************************************************************/
-    Vertex* v0 = m->getVertexVector()[0];
-    size_t numNeighborV0 = v0->getNeighborNum();
-    ASSERT_EQ(numNeighborV0, 4) << "The 0th vertex does not have the right number of neighbors.";
-
-    // Assign force to 0th vertex
-    v0->force[2] = -radius;
-
-    m->updateGeometry(false, 1.0); // Moves the 0th vertex to the center of the octahedron.
-
-    // Check edge length
-    double exStretchedEdgeLen = radius;
-    for(Edge* it: v0->getNeighborEdges())
-        EXPECT_DOUBLE_EQ(it->getMEdge()->getStretchedLength(), exStretchedEdgeLen);
-    // Check triangle area and angle
-    double exStretchedTriangleArea = radius * radius / 2;
-    double exStretchedTriangleAngleIn = M_PI / 2;
-    double exStretchedTriangleAngleOut = M_PI / 4;
-    for(size_t nIdx = 0; nIdx < numNeighborV0; ++nIdx) {
-        EXPECT_DOUBLE_EQ(v0->getNeighborTriangles()[nIdx]->getMTriangle()->getStretchedArea(), exStretchedTriangleArea);
-        for(size_t angleIdx = 0; angleIdx < 3; ++angleIdx) {
-            if((3 - v0->getTriangleHead()[nIdx]) % 3 == angleIdx)
-                EXPECT_DOUBLE_EQ(
-                    v0->getNeighborTriangles()[nIdx]->getMTriangle()->getStretchedTheta()[angleIdx],
-                    exStretchedTriangleAngleIn
-                );
-            else
-                EXPECT_DOUBLE_EQ(
-                    v0->getNeighborTriangles()[nIdx]->getMTriangle()->getStretchedTheta()[angleIdx],
-                    exStretchedTriangleAngleOut
-                );
-        }
-    }
-    // Check Voronoi cell area and curvature
-    double exStretchedVCellArea = radius * radius;
-    double exStretchedVCellCurv = 0;
-    EXPECT_DOUBLE_EQ(v0->getMVoronoiCell()->getStretchedArea(), exStretchedVCellArea);
-    EXPECT_DOUBLE_EQ(v0->getMVoronoiCell()->getStretchedCurv(), exStretchedVCellCurv);
-
-}
-
-TEST_F(MembraneFFTest, Derivative) {
-    m->updateGeometry(true);
-    recordCoordinate(m);
-    assignRandomForce(m, radius/200); // Simple test shows that 100 induces a change not small enough
-
-    size_t numEdges = m->getEdgeVector().size();
-    size_t numTriangles = m->getTriangleVector().size();
-    size_t numVertices = m->getVertexVector().size();
-
-    // Then move every vertex a little bit
-    moveAlongForce(m, 1.0);
-    m->updateGeometry(false, 0.0); // use stretched calculation here to speed things up
+    resizeEqArea(m, 0.9); // This is to shrink the equilibrium area by a little bit to avoid zero forces.
     
-    vector<double> edgeLength1(numEdges);
-    for(size_t idx = 0; idx < numEdges; ++idx) {
-        edgeLength1[idx] = m->getEdgeVector()[idx]->getMEdge()->getStretchedLength();
-    }
-    vector<double> triangleArea1(numTriangles);
-    vector<array<double, 3>> triangleTheta1(numTriangles, {{}});
-    vector<array<double, 3>> triangleCotTheta1(numTriangles, {{}});
-    for(size_t idx = 0; idx < numTriangles; ++idx) {
-        triangleArea1[idx] = m->getTriangleVector()[idx]->getMTriangle()->getStretchedArea();
-        triangleTheta1[idx] = m->getTriangleVector()[idx]->getMTriangle()->getStretchedTheta();
-        triangleCotTheta1[idx] = m->getTriangleVector()[idx]->getMTriangle()->getStretchedCotTheta();
-    }
-    vector<double> vCellArea1(numVertices);
-    vector<double> vCellCurv1(numVertices);
-    for(size_t idx = 0; idx < numVertices; ++idx) {
-        vCellArea1[idx] = m->getVertexVector()[idx]->getMVoronoiCell()->getStretchedArea();
-        vCellCurv1[idx] = m->getVertexVector()[idx]->getMVoronoiCell()->getStretchedCurv();
-    }
+    double mSTriangleU = mSTriangle.computeEnergy(0.0);
+    double mSVoronoiU = mSVoronoi.computeEnergy(0.0);
+    EXPECT_NEAR(mSTriangleU, mSVoronoiU, abs(mSTriangleU + mSVoronoiU) * 1e-9);
 
-    // Now move every vertex in the opposite direction
-    resetCoordinate(m);
-    moveAlongForce(m, -1.0);
-    m->updateGeometry(false, 0.0);
+}
 
-    vector<double> edgeLength2(numEdges);
-    for(size_t idx = 0; idx < numEdges; ++idx) {
-        edgeLength2[idx] = m->getEdgeVector()[idx]->getMEdge()->getStretchedLength();
-    }
-    vector<double> triangleArea2(numTriangles);
-    vector<array<double, 3>> triangleTheta2(numTriangles, {{}});
-    vector<array<double, 3>> triangleCotTheta2(numTriangles, {{}});
-    for(size_t idx = 0; idx < numTriangles; ++idx) {
-        triangleArea2[idx] = m->getTriangleVector()[idx]->getMTriangle()->getStretchedArea();
-        triangleTheta2[idx] = m->getTriangleVector()[idx]->getMTriangle()->getStretchedTheta();
-        triangleCotTheta2[idx] = m->getTriangleVector()[idx]->getMTriangle()->getStretchedCotTheta();
-    }
-    vector<double> vCellArea2(numVertices);
-    vector<double> vCellCurv2(numVertices);
-    for(size_t idx = 0; idx < numVertices; ++idx) {
-        vCellArea2[idx] = m->getVertexVector()[idx]->getMVoronoiCell()->getStretchedArea();
-        vCellCurv2[idx] = m->getVertexVector()[idx]->getMVoronoiCell()->getStretchedCurv();
-    }
+TEST_F(MembraneFFTest, Force) {
+    MembraneStretching<MembraneStretchingHarmonic> mSTriangle;
+    MembraneStretching<MembraneStretchingVoronoiHarmonic> mSVoronoi;
+    MembraneBending<MembraneBendingVoronoiHelfrich> mBVoronoi;
+    vector<MembraneInteractions*> memInteraction = {&mSTriangle, &mSVoronoi, &mBVoronoi};
 
-    // Compare the results with derivative predictions
-    // A(x+h) - A(x-h) = dotProduct(2h, dA/dx)
-    for(size_t idx = 0; idx < numEdges; ++idx) {
-        Edge* e = m->getEdgeVector()[idx];
-        // Edge length
+    recordCoordinate(m);
+    assignRandomForceAuxP(m, radius/200);
+    resizeEqArea(m, 0.9); // This is to shrink the equilibrium area by a little bit to avoid zero forces.
+
+    // Compare the results with force predictions
+    // U(x+h) - U(x-h) = dotProduct(2h, dU/dx) = -dotProduct(2h, F)
+
+    for(auto eachMemInteraction: memInteraction) {
+        resetCoordinate(m);
+        resetForce(m);
+        m->updateGeometry(true);
+        eachMemInteraction->computeForces();
+
+        moveAlongForceAuxP(m, 1.0);
+        m->updateGeometry(true);
+        double U1 = eachMemInteraction->computeEnergy(0.0);
+
+        resetCoordinate(m);
+        moveAlongForceAuxP(m, -1.0);
+        m->updateGeometry(true);
+        double U2 = eachMemInteraction->computeEnergy(0.0);
+
         double exDiff = 0.0;
-        for(size_t vIdx = 0; vIdx < 2; ++vIdx) {
-            exDiff += 2 * dotProduct(
-                e->getVertices()[vIdx]->force,
-                array2Vector<double, 3>(e->getMEdge()->getDLength()[vIdx])
-            );
-        }
-        EXPECT_NEAR(edgeLength1[idx] - edgeLength2[idx], exDiff, abs(exDiff / 1000));
-    }
-	for(size_t idx = 0; idx < numTriangles; ++idx) {
-        Triangle *t = m->getTriangleVector()[idx];
-        // Triangle area
-        double exDiff = 0.0;
-        for(size_t vIdx = 0; vIdx < 3; ++vIdx) {
-            exDiff += 2 * dotProduct(
-                t->getVertices()[vIdx]->force,
-                array2Vector<double, 3>(t->getMTriangle()->getDArea()[vIdx])
-            );
-        }
-        EXPECT_NEAR(triangleArea1[idx] - triangleArea2[idx], exDiff, abs(exDiff / 1000));
-        // Triangle angles
-        for(size_t aIdx = 0; aIdx < 3; ++aIdx) {
-            exDiff = 0.0;
-            for(size_t vIdx = 0; vIdx < 3; ++vIdx) {
-                exDiff += 2 * dotProduct(
-                    t->getVertices()[vIdx]->force,
-                    array2Vector<double, 3>(t->getMTriangle()->getDTheta()[aIdx][vIdx])
-                );
-            }
-            EXPECT_NEAR(triangleTheta1[idx][aIdx] - triangleTheta2[idx][aIdx], exDiff, abs(exDiff / 1000));
-        }
-        // Triangle angles (cot)
-        for(size_t aIdx = 0; aIdx < 3; ++aIdx) {
-            exDiff = 0.0;
-            for(size_t vIdx = 0; vIdx < 3; ++vIdx) {
-                exDiff += 2 * dotProduct(
-                    t->getVertices()[vIdx]->force,
-                    array2Vector<double, 3>(t->getMTriangle()->getDCotTheta()[aIdx][vIdx])
-                );
-            }
-            EXPECT_NEAR(triangleCotTheta1[idx][aIdx] - triangleCotTheta2[idx][aIdx], exDiff, abs(exDiff / 1000));
-        }
-    }
-    for(size_t idx = 0; idx < numVertices; ++idx) {
-        Vertex *v = m->getVertexVector()[idx];
-        size_t numNeighbor = v->getNeighborNum();
-        // Voronoi cell area
-        double exDiff = 0.0;
-        exDiff += 2 * dotProduct(
-            v->force,
-            array2Vector<double, 3>(v->getMVoronoiCell()->getDArea())
-        );
-        for(size_t vIdx = 0; vIdx < numNeighbor; ++vIdx) {
-            exDiff += 2 * dotProduct(
-                v->getNeighborVertices()[vIdx]->force,
-                array2Vector<double, 3>(v->getMVoronoiCell()->getDNeighborArea()[vIdx])
-            );
-        }
-        EXPECT_NEAR(vCellArea1[idx] - vCellArea2[idx], exDiff, abs(exDiff / 1000));
-        // Voronoi cell curvature
-        exDiff = 0.0;
-        exDiff += 2 * dotProduct(
-            v->force,
-            array2Vector<double, 3>(v->getMVoronoiCell()->getDCurv())
-        );
-        for(size_t vIdx = 0; vIdx < numNeighbor; ++vIdx) {
-            exDiff += 2 * dotProduct(
-                v->getNeighborVertices()[vIdx]->force,
-                array2Vector<double, 3>(v->getMVoronoiCell()->getDNeighborCurv()[vIdx])
-            );
-        }
-        EXPECT_NEAR(vCellCurv1[idx] - vCellCurv2[idx], exDiff, abs(exDiff / 1000));
+        for(Vertex* v: m->getVertexVector())
+            exDiff -= 2 * dotProduct(v->forceAuxP, v->force);
+
+        EXPECT_NEAR(U1 - U2, exDiff, abs(exDiff / 1000))
+            << eachMemInteraction->getName() << " force not working properly.";
+    
     }
 
 }
 
 
-#  endif //DO_THIS_GEOMETRY_MEMBRANE_TEST
+#  endif //DO_THIS_FF_MEMBRANE_TEST
 #endif //TESTING
 
