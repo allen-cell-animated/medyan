3965e157aed9c0f95bc3a6b2c1597223f831c982
drelatbitbucket <drel.sohu.com@gmail.com>
2018-01-03 01:09:53 -0500

    Revised tri-cyl vol ff test. Parameters still need to be adjusted.

--- src/TESTS/test_ff_triangle_cylinder_volume.cpp
+++ src/TESTS/test_ff_triangle_cylinder_volume.cpp
@@ -18,8 +18,6 @@
 
 #    include "gtest/gtest.h"
 
-#    include <random>
-
 #    include "common.h"
 #    include "test_public.h"
 #    include "MathFunctions.h"
@@ -104,100 +102,87 @@ namespace {
             }
         }
 
-    };
-
-    void recordCoordinate(Membrane *m) {
-        for(Vertex* it: m->getVertexVector()) it->coordinateP = it->coordinate;
-    }
-    void resetCoordinate(Membrane *m) {
-        for(Vertex* it: m->getVertexVector()) it->coordinate = it->coordinateP;
-    }
-    void resetForce(Membrane *m) {
-        for(Vertex* it: m->getVertexVector()) it->force.assign(3, 0);
-    }
-    void assignRandomForceAuxP(Membrane* m, double sigma) {
+        void recordCoordinate() {
+            for(Vertex* it: tv) it->coordinateP = it->coordinate;
+            for(Bead* it: cb) it->coordinateP = it->coordinate;
+        }
+        void resetCoordinate() {
+            for(Vertex* it: tv) it->coordinate = it->coordinateP;
+            for(Bead* it: cb) it->coordinate = it->coordinateP;
+        }
+        void resetForce() {
+            for(Vertex* it: tv) it->force.assign(3, 0);
+            for(Bead* it: cb) it->force.assign(3, 0);
+        }
+        void assignRandomForceAuxP(double sigma) {
 		normal_distribution<> nd(0, sigma);
 
-		for(Vertex* it: m->getVertexVector()) {
-			for(double& eachForce: it->forceAuxP) { // forceAuxP is used here simply because it is an empty container.
-				eachForce = nd(Rand::engFixed);
-			}
-		}
-    }
-    void moveAlongForceAuxP(Membrane* m, double d) {
-        for(Vertex* it: m->getVertexVector()) {
-            for(size_t coordIdx = 0; coordIdx < 3; ++coordIdx) {
-                it->coordinate[coordIdx] += it->forceAuxP[coordIdx] * d;
+            for(Vertex* it: tv) {
+                for(double& eachForce: it->forceAuxP) { // forceAuxP is used here simply because it is an empty container.
+                    eachForce = nd(Rand::engFixed);
+                }
+            }
+            for(Bead* it: cb) {
+                for(double& eachForce: it->forceAuxP) { // forceAuxP is used here simply because it is an empty container.
+                    eachForce = nd(Rand::engFixed);
+                }
             }
         }
-    }
-    void resizeEqArea(Membrane *m, double ratio) {
-        for(Triangle* t: m->getTriangleVector()) {
-            t->getMTriangle()->setEqArea(t->getMTriangle()->getEqArea() * ratio);
+        void moveAlongForceAuxP(double d) {
+            for(Vertex* it: tv) {
+                for(size_t coordIdx = 0; coordIdx < 3; ++coordIdx) {
+                    it->coordinate[coordIdx] += it->forceAuxP[coordIdx] * d;
+                }
+            }
+            for(Bead* it: cb) {
+                for(size_t coordIdx = 0; coordIdx < 3; ++coordIdx) {
+                    it->coordinate[coordIdx] += it->forceAuxP[coordIdx] * d;
+                }
+            }
         }
-        for(Vertex* v: m->getVertexVector()) {
-            v->getMVoronoiCell()->setEqArea(v->getMVoronoiCell()->getEqArea() * ratio);
-        }
-    }
-}
 
-TEST_F(TriangleCylinderVolumeFFTest, CompareStretchingEnergy) {
-
-    // Check that the two different methods give the same stretching energy.
-    MembraneStretching<MembraneStretchingHarmonic> mSTriangle;
-    MembraneStretching<MembraneStretchingVoronoiHarmonic> mSVoronoi;
-
-    resizeEqArea(m, 0.9); // This is to shrink the equilibrium area by a little bit to avoid zero forces.
-    
-    double mSTriangleU = mSTriangle.computeEnergy(0.0);
-    double mSVoronoiU = mSVoronoi.computeEnergy(0.0);
-    EXPECT_NEAR(mSTriangleU, mSVoronoiU, abs(mSTriangleU + mSVoronoiU) * 1e-9);
+    };
 
 }
 
 TEST_F(TriangleCylinderVolumeFFTest, Force) {
-    MembraneStretching<MembraneStretchingHarmonic> mSTriangle;
-    MembraneStretching<MembraneStretchingVoronoiHarmonic> mSVoronoi;
-    MembraneBending<MembraneBendingVoronoiHelfrich> mBVoronoi;
-    vector<MembraneInteractions*> memInteraction = {&mSTriangle, &mSVoronoi, &mBVoronoi};
+    TriangleCylinderExclVolume<TriangleCylinderBeadExclVolRepulsion> tcv;
 
-    assignRandomForceAuxP(m, radius/500);
-    resizeEqArea(m, 0.9); // This is to shrink the equilibrium area by a little bit to avoid zero forces.
-    moveAlongForceAuxP(m, 2.5); // Also, previous tests show that the right octahedron shape has the minimum
-                                // bending energy, so we also need to distort the shape a little bit, to avoid
-                                // zero force of bending energy.
+    assignRandomForceAuxP(radius/200);
     recordCoordinate(m);
 
     // Compare the results with force predictions
     // U(x+h) - U(x-h) = dotProduct(2h, dU/dx) = -dotProduct(2h, F)
 
-    for(auto eachMemInteraction: memInteraction) {
-        resetCoordinate(m);
-        resetForce(m);
-        m->updateGeometry(true);
-        eachMemInteraction->computeForces();
+    resetCoordinate();
+    resetForce();
+    // Update position for neighbor list search
+    t->updatePosition();
+    c->updatePosition();
+    t->updateGeometry(true);
+    tcv->computeForces();
 
-        moveAlongForceAuxP(m, 1.0);
-        m->updateGeometry(true);
-        double U1 = eachMemInteraction->computeEnergy(0.0);
+    // Don't bother updating neighbor list here
+    moveAlongForceAuxP(m, 1.0);
+    t->updateGeometry(true);
+    double U1 = tcv->computeEnergy(0.0);
 
-        resetCoordinate(m);
-        moveAlongForceAuxP(m, -1.0);
-        m->updateGeometry(true);
-        double U2 = eachMemInteraction->computeEnergy(0.0);
+    resetCoordinate(m);
+    moveAlongForceAuxP(m, -1.0);
+    m->updateGeometry(true);
+    double U2 = tcv->computeEnergy(0.0);
 
-        double exDiff = 0.0;
-        for(Vertex* v: m->getVertexVector())
-            exDiff -= 2 * dotProduct(v->forceAuxP, v->force);
+    double exDiff = 0.0;
+    for(Vertex* v: m->getVertexVector())
+        exDiff -= 2 * dotProduct(v->forceAuxP, v->force);
 
-        EXPECT_NEAR(U1 - U2, exDiff, abs(exDiff / 1000))
-            << eachMemInteraction->getName() << " force not working properly.";
+    EXPECT_NEAR(U1 - U2, exDiff, abs(exDiff / 1000))
+        << tcv->getName() << " force not working properly.";
     
-    }
 
 }
 
 
-#  endif //DO_THIS_FF_MEMBRANE_TEST
-#endif //TESTING
+#  endif // DO_THIS_FF_TRIANGLE_CYLINDER_VOLUME_TEST
+#endif // TESTING
 
