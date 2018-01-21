6c76f25586e00f8b1d00c533856630db5d321b12
drelatbitbucket <drel.sohu.com@gmail.com>
2018-01-21 13:57:54 -0500

    Starting to slice those triangles.

--- src/Structure/SurfaceMesh/MeshSlicing.cpp
+++ src/Structure/SurfaceMesh/MeshSlicing.cpp
@@ -14,7 +14,7 @@ void MeshSlicingManager::planeSliceTriangle(size_t aspect, double otherCoord, Tr
         - Triangles will be sliced and contain 0 or more more 3d polygons
         - A plane slicing snapshot will be created
     **************************************************************************/
-    
+
     // Check whether this triangle can be cut
     auto& v0 = triangle->getVertices()[0]->coordinate;
     auto& v1 = triangle->getVertices()[1]->coordinate;
@@ -24,6 +24,22 @@ void MeshSlicingManager::planeSliceTriangle(size_t aspect, double otherCoord, Tr
         return; // No need to cut this triangle
     
     // Check whether this triangle has already been cut
+    auto& po = triangle->getGTriangle()->getPolygons();
+    if(po.size() == 0) {
+        // Create the first polygon using this triangle
+        auto sv0 = make_unique<SimpleVertex<3>>(vector2Array<double, 3>(v0));
+        auto sv1 = make_unique<SimpleVertex<3>>(vector2Array<double, 3>(v1));
+        auto sv2 = make_unique<SimpleVertex<3>>(vector2Array<double, 3>(v2));
+        po.emplace_back({{sv0.get(), sv1.get(), sv2.get()}});
+
+        vertices3.push_back(move(sv0));
+        vertices3.push_back(move(sv1));
+        vertices3.push_back(move(sv2));
+    }
+
+    for(size_t idx = 0; idx < po.size(); ++idx) { // Note that now size() could change.
+
+    }
     // TODO: implementation
 }
 
