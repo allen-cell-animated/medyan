617de2dcf26ec1a99ac042f0fc466b9b04e1cdf3
drelatbitbucket <drel.sohu.com@gmail.com>
2018-01-24 16:37:33 -0500

    Made the framework for calculating cube slicing.

--- src/Structure/CubeSlicing.h
+++ src/Structure/CubeSlicing.h
@@ -14,9 +14,9 @@ struct PlaneCubeSlicingResult {
 
     std::array<double, 6> areaIn;
     // in the order of
-    // (x_min, y, z), (x_max, y, z),
-    // (x, y_min, z), (x, y_max, z),
-    // (x, y, z_min), (x, y, z_max)
+    // x = x_min, x = x_max,
+    // y = y_min, y = y_max,
+    // z = z_min, z = z_max
 
     PlaneCubeSlicingResult& operator*=(double a) {
         // Expand the volume and area
@@ -26,14 +26,86 @@ struct PlaneCubeSlicingResult {
         return *this;
     }
 
+    PlaneCubeSlicingResult& flip(unsigned short int flippedCoords) { // coords (0-7) in binary is zyx
+        for(size_t inspect = 0; inspect < 3; ++inspect) {
+            if((flippedCoords >> inspect) & 1) {
+                std::swap(areaIn[inspect*2], areaIn[inspect*2+1]);
+            }
+        }
+        return *this;
+    }
+    PlaneCubeSlicingResult& reverse(double a) { // a is cube size
+        double a2 = a * a;
+        volumeIn = a2 * a - volumeIn;
+        for(double& eachAreaIn: areaIn) eachAreaIn = a2 - eachAreaIn;
+        return *this;
+    }
+
 };
 
 inline PlaneCubeSlicingResult planeUnitCubeSlice( // Cube [0, 1] x [0, 1] x [0, 1]
     const std::array<double, 3>& point,  // A point on the plane
     const std::array<double, 3>& normal, // Unit normal of the plane pointing outwards
 ) {
-    PlaneCubeSlicingResult res;
-    // TODO: implementation
+    PlaneCubeSlicingResult res{}; // zero-initiate
+
+    // First consider the case where normal has only non-neg components
+    std::array<double, 3> flippedPoint = point;
+    std::array<double, 3> flippedNormal = normal;
+    unsigned short int flippedCoords = 0;
+    for(size_t idx = 0; idx < 3; ++idx) {
+        if(flippedNormal[idx] < 0) {
+            flippedCoords += (1 << idx);
+            flippedPoint[idx] = 1 - flippedPoint[idx];
+            flippedNormal[idx] = -flippedNormal[idx];
+        }
+    }
+
+    double signedDist = dotProduct(flippedPoint, flippedNormal);
+    double signedDistMax = dotProduct(std::array<double, 3>{{1, 1, 1}}, flippedNormal);
+
+    bool reverse = false;
+
+    if(signedDist <= 0) {
+        // do nothing because everything is zero
+    } else if(signedDist >= signedDistMax) {
+        res.volumeIn = 1;
+        res.areaIn = {{1, 1, 1, 1, 1, 1}};
+    } else {
+        if(signedDist > 0.5 * signedDistMax) {
+            // Doing a full flip, plus swapping inside/outside,
+            // which leaves the normal unchanged.
+            reverse = true;
+            flippedPoint ^= ((1 << 3) - 1); // full flip
+            for(size_t idx = 0; idx < 3; ++idx) {
+                flippedPoint[idx] = 1 - flippedPoint[idx];
+            }
+
+            signedDist = signedDistMax - signedDist;
+        }
+
+        // Now the signedDist should be within [0, 0.5 signedDistMax]
+
+        // Find the intersections on the axis. The values should be within [0, +inf]
+        double x = signedDistance / flippedNormal[0];
+        double y = signedDistance / flippedNormal[1];
+        double z = signedDistance / flippedNormal[2];
+
+        if(x <= 1 && y <= 1 && z <= 1) {
+            res.volumeIn = x * y * z / 6;
+            res.areaIn[0] = y * z / 2;
+            res.areaIn[2] = z * x / 2;
+            res.areaIn[4] = x * y / 2;
+        } else if(x > 1 && y <= 1 && z <= 1) {
+            // TODO: implementation
+        }
+    }
+
+    // Finally restore the flipping
+    res.flip(flippedCoords);
+    if(reverse) res.reverse(1);
+
+    return res;
 }
 
 inline PlaneCubeSlicingResult planeCubeSlice(
