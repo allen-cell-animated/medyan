6971e75eb8831c13ed9970fecec97146731deab9
drelatbitbucket <drel.sohu.com@gmail.com>
2018-01-26 14:33:24 -0500

    Finished cube slicing tests.

--- src/TESTS/test_cube_slicing.cpp
+++ src/TESTS/test_cube_slicing.cpp
@@ -113,6 +113,109 @@ TEST(CubeSlicingTest, UnitCubeFlipping) {
     /**************************************************************************
     Test the flipping of normal vector
     **************************************************************************/
+
+    const double nVal = 1.0 / sqrt(3);
+    const double pVal = 1.0 / 6.0;
+
+    auto r1 = planeUnitCubeSlice({{pVal, pVal, pVal}}, {{nVal, nVal, nVal}});
+
+    // Flip x
+    {
+        std::array<size_t, 6> comp {{1, 0, 2, 3, 4, 5}};
+        auto r2 = planeUnitCubeSlice({{1-pVal, pVal, pVal}}, {{-nVal, nVal, nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+    // Flip y
+    {
+        std::array<size_t, 6> comp {{0, 1, 3, 2, 4, 5}};
+        auto r2 = planeUnitCubeSlice({{pVal, 1-pVal, pVal}}, {{nVal, -nVal, nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+    // Flip z
+    {
+        std::array<size_t, 6> comp {{0, 1, 2, 3, 5, 4}};
+        auto r2 = planeUnitCubeSlice({{pVal, pVal, 1-pVal}}, {{nVal, nVal, -nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+    // Flip yz
+    {
+        std::array<size_t, 6> comp {{0, 1, 3, 2, 5, 4}};
+        auto r2 = planeUnitCubeSlice({{pVal, 1-pVal, 1-pVal}}, {{nVal, -nVal, -nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+    // Flip zx
+    {
+        std::array<size_t, 6> comp {{1, 0, 2, 3, 5, 4}};
+        auto r2 = planeUnitCubeSlice({{1-pVal, pVal, 1-pVal}}, {{-nVal, nVal, -nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+    // Flip xy
+    {
+        std::array<size_t, 6> comp {{1, 0, 3, 2, 4, 5}};
+        auto r2 = planeUnitCubeSlice({{1-pVal, 1-pVal, pVal}}, {{-nVal, -nVal, nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+    // Flip xyz
+    {
+        std::array<size_t, 6> comp {{1, 0, 3, 2, 5, 4}};
+        auto r2 = planeUnitCubeSlice({{1-pVal, 1-pVal, 1-pVal}}, {{-nVal, -nVal, -nVal}});
+        EXPECT_DOUBLE_EQ(r1.volumeIn, r2.volumeIn);
+        for(size_t i = 0; i < 6; ++i) {
+            EXPECT_DOUBLE_EQ(r1.areaIn[i], r2.areaIn[comp[i]]);
+        }
+    }
+
+}
+
+TEST(CubeSlicingTest, TranslationAndScaling) {
+    /**************************************************************************
+    Test the slicing some cube with certain position and size
+    **************************************************************************/
+
+    const double nVal = 1.0 / sqrt(3);
+    const double pVal = 1.0 / 6.0;
+    const double r0Val = 10.0;
+    const double s = 2.0;
+
+    auto r = planeCubeSlice(
+        {{r0Val + s * pVal, r0Val + s * pVal, r0Val + s * pVal}},
+        {{-nVal, -nVal, -nVal}},
+        {{r0Val, r0Val, r0Val}},
+        s
+    );
+
+    const double exVolumeIn = 47.0 / 6.0;
+    const std::array<double, 6> exAreaIn {{3.5, 4.0, 3.5, 4.0, 3.5, 4.0}};
+
+    EXPECT_NEAR(r.volumeIn, exVolumeIn, abs(exVolumeIn) * 1e-5);
+    for(size_t i = 0; i < 6; ++i) {
+        EXPECT_NEAR(r.areaIn[i], exAreaIn[i], abs(exAreaIn[i]) * 1e-5);
+    }
+
 }
 
 #  endif
