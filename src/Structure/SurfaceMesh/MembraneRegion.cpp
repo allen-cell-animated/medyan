38b6603fb3dbc1c2f05d0232f7cd30f58131f78d
drelatbitbucket <drel.sohu.com@gmail.com>
2018-02-19 19:02:01 -0500

    Modified slightly the implementation of regions.

--- src/Structure/SurfaceMesh/MembraneRegion.cpp
+++ src/Structure/SurfaceMesh/MembraneRegion.cpp
@@ -8,16 +8,31 @@ MembraneRegion::MembraneRegion(MembraneHierarchy* hier, bool excludeChilden):
     _hierOut({hier})
 {
     if(excludeChildren) {
-        // TODO
+        size_t n = hier->numberOfChildren();
+        _hierIn.reserve(n);
+        for(size_t idx = 0; idx < n; ++idx) {
+            _hierIn.push_back( static_cast<MembraneHierarchy*>(hier->children(idx)) );
+        }
     }
 }
 
 MembraneRegion::MembraneRegion(Boundary* b, MembraneHierarchy* parentOfExcluded):
     _boundary(b)
 {
-    // TODO
+    size_t n = parentOfExcluded->numberOfChildren();
+    _hierIn.reserve(n);
+    for(size_t idx = 0; idx < n; ++idx) {
+        _hierIn.push_back( static_cast<MembraneHierarchy*>(parentOfExcluded->children(idx)) );
+    }
 }
 
 bool MembraneRegion::contains(const std::array<double, 3>& point) {
+    /**************************************************************************
+    This function checks whether a certain point is in the region. If a point
+    is in the region, it must satisfy all of the following.
+        - within _boundary if specified
+        - in any of the membranes in _hierIn
+        - not in any of the membranes in _hierOut
+    **************************************************************************/
     // TODO
 }
