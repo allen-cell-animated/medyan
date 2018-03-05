a19ff8a4b7618c087fbbdd456afdfa65c89c9589
drelatbitbucket <drel.sohu.com@gmail.com>
2018-03-05 14:45:48 -0500

    Changed command line parser (not finished).

--- src/core/command_line_parser.cpp
+++ src/core/command_line_parser.cpp
@@ -1,22 +1,82 @@
 #include "core/command_line_parser.h"
 
+#include <algorithm>
+#include <sstream>
+
 namespace medyan {
 
-namespace
-bool CommandLineOptionBase::findHit(const std::string& arg, CommandLineParser::ArgType argType) {
-
-}
-void CommandLineParser::parse(int argc, char** argv) {
+bool CommandLineParser::parse(int argc, char** argv) {
     std::vector<char*> unusedArgs;
 
     for(int idx = 1; idx < argc; ++idx) {
+        ArgType t = getArgType(argv[idx]);
         std::string arg {argv[idx]};
+        if(t == ArgType::Fail) {
+            _invalidArgContent = arg;
+            _invalidArg = true;
+            return false;
+        }
         
         for(auto& opPtr: _op) {
-            int requireArgs = 0;
-            opPtr->findHit(arg, requireArgs);
+            if(opPtr->findHit(arg, t)) {
+                opPtr->evaluate(argc, argv, idx); // idx may be changed in this function
+                // TODO
+
+                if(t != ArgType::Short) break;
+                // Short args may be evaluated by other options, e.g. "-af" would be evaluated by "-a" and "-f"
+            }
+
         }
     }
-};
+}
+
+
+void CommandLineOptionBase::_preprocess() {
+    int prevLoc = 0;
+    std::string eachStr;
+    std::istringstream iss{std::string(_match)};
+    while(std::getline(iss, eachStr, ',')) {
+        size_t len = eachStr.length();
+        if(len == 2 && eachStr[0] == '-' && eachStr[1] != '-') {
+            // Short flag
+            _flagShort = eachStr[1];
+        } else if(len > 2 && std::string(eachStr, 0, 2) == "--") {
+            // Long flag
+            _flagLong = std::string(eachStr, 2);
+        } else if(len && eachStr[0] != '-') {
+            // Command flag
+            _flagCommand = eachStr;
+        } else {
+            // Failed
+            _inputFailBit = true;
+        }
+    }
+}
+
+bool CommandLineOptionBase::findHit(const std::string& arg, CommandLineParser::ArgType argType) {
+    switch(argType) {
+    case CommandLineParser::ArgType::Short:
+        if(_flagShort) {
+            size_t pos = arg.find(_flagShort);
+            if(pos != std::string::npos) {
+                if(_numArgs) {
+                    if(pos == arg.length() - 1) return true;
+                }
+                else return true;
+            }
+        }
+        break;
+    case CommandLineParser::ArgType::Long:
+        if(_flagLong == std::string(arg, 2)) return true;
+        break;
+    case CommandLineParser::ArgType::Argument:
+        if(_flagCommand == arg) return true;
+        break;
+    default:
+        break;
+    }
+
+    return false;
+}
 
 } // namespace medyan
--- src/core/command_line_parser.h
+++ src/core/command_line_parser.h
@@ -8,8 +8,44 @@
 
 namespace medyan {
 
+// Forward declarations
 class CommandLineOptionBase;
-class CommandLineParser;
+
+class CommandLineParser {
+private:
+    std::vector<std::unique_ptr<CommandLineOptionBase>> _op;
+
+    /// Fail flags and associated variables
+    bool _invalidArg = false;
+    std::string _invalidArgContent;
+
+public:
+    enum class ArgType {
+        Short, // Short option (eg "-a") or combined short option (eg "-af"). No more '-' is allowed
+        Long, // Long option (eg "--analysis").
+        Argument, // Argument or sub-command, not starting with "-" or "--" (eg "23" or "run")
+        Fail // Unsupported syntax (eg "-a-f")
+    };
+
+    /// Helper function to get arg type
+    static ArgType getArgType(const char* argv) {
+        int i = 0;
+        ArgType t;
+        bool startHyphen = false;
+        bool startHyphen2 = false;
+        while(argv[i]) {
+            if(i == 0 && argv[i] == '-') startHyphen = true;
+            if(i == 1 && startHyphen && argv[i] == '-') startHyphen2 = true;
+            if(i > 1 && startHyphen && !startHyphen2 && argv[i] == '-') return ArgType::Fail;
+            ++i;
+        }
+        if(startHyphen2) return ArgType::Long;
+        if(startHyphen) return ArgType::Short;
+        return ArgType::Argument;
+    }
+
+    bool parse(int argc, char** argv);
+};
 
 class CommandLineOptionBase {
 protected:
@@ -19,13 +55,22 @@ protected:
 
     CommandLineParser* _p;
 
-    /// Fail bits
-    bool _badinput = false;
+    /// Preprocessing
+    char _flagShort = 0; // e.g. "-a" w/o "-"
+    std::string _flagLong; // e.g. "--analysis" w/o "--"
+    std::string _flagCommand; // e.g. "add"
+    virtual void _preprocess();
+
+    /// Fail flags and associated info
+    bool _inputFailBit = false; // input parsing failure
+    bool _endOfArgListBit = false; // end of argument list when reading arguments
+    bool _invalidArgBit = false; // Invalid argument
+    std::string _invalidArgContent;
 
     /// Configuration of the option
     bool _required = false;
     std::string _requiredNotFulfilled {""};
-    int _requiredArgs = 0;
+    int _numArgs = 0;
 
     /// Evaluate
     bool _evaluated = false;
@@ -33,11 +78,16 @@ protected:
 public:
     /// Check state
     operator bool()const {
-        return _badinput;
+        return !(_inputFailBit || _endOfArgListBit || _invalidArgBit);
     }
 
     /// Getters
-    int getRequiredArgs()const { return _requiredArgs; }
+    int getRequiredArgs()const { return _numArgs; }
+
+    bool getInputFailBit()const { return _inputFailBit; }
+    bool getEndOfArgListBit()const { return _endOfArgListBit; }
+    bool getInvalidArgBit()const { return _invalidArgBit; }
+    const std::string& getInvalidArgContent()const { return _invalidArgContent; }
 
     /// Change the option behavior. These manipulators always return the class itself.
     virtual CommandLineOptionBase& required(const std::string& errorMessage) {
@@ -46,13 +96,17 @@ public:
         return *this;
     }
 
-    /// Validate. Options with arguments should override this function
-    virtual bool validate() { return true; }
-
     /// Find hit.
     virtual bool findHit(const std::string& arg, CommandLineParser::ArgType argType);
+
+    /// Evaluate
+    virtual void evaluate(int argc, char** argv, int& argp) = 0;
+
+    /// Validate. Options with arguments should override this function
+    virtual bool validate() { return true; }
 };
 
+/// Command line option which takes exactly one argument
 template<typename T>
 class CommandLineOption: public CommandLineOptionBase {
 private:
@@ -65,21 +119,54 @@ private:
 public:
     CommandLineOption(const char* match, T* destination, const std::string& description):
         _match(match), _value(destination), _description(description),
-        _requiredArguments(1) {}
+        _numArgs(1)
+    {
+        _preprocess();
+    }
     CommandLineOption(const char* match, T* destination, std::function<bool(T)> validator, const std::string& description):
         _match(match), _value(destination), _description(description), _validator(validator),
-        _requiredArguments(1) {}
+        _numArgs(1)
+    {
+        _preprocess();
+    }
+
+    /// Evaluate
+    virtual void evaluate(int argc, char** argv, int& argp)override {
+        // _numArgs == 1
+        if(argp + 1 >= argc) {
+            _endOfArgListBit = true;
+            return;
+        }
+
+        ++argp;
+        std::istringstream iss(argv[argp]);
+        iss >> _value;
+        if(iss.fail()) {
+            _invalidArgBit = true;
+            _invalidArgContent = std::string(argv[argp]);
+        }
+    }
 
     /// Validate.
     virtual bool validate()override { return !_validator || _validator(*_value); }
 
 };
 
+/// Command line option which takes no arguments
 class CommandLineFlag: public CommandLineOptionBase {
     /// The flag value
     bool* _value;
+    bool _turnOn = true; ///< _value is true or false when this flag exists.
+
 public:
-    CommandLineFlag(const char* match, bool* destination, const std::string& description);
+    CommandLineFlag(const char* match, bool* destination, bool turnOn, const std::string& description):
+        _match(match), _value(destination), _turnOn(turnOn), _description(description)
+    {
+        _preprocess();
+    }
+
+    /// Evaluate
+    virtual void evaluate(int argc, char** argv, int& argp)override { *_value = _turnOn; }
 };
 
 /* Broken type... Do not use it for now. */
@@ -99,38 +186,6 @@ public:
     }
 };
 
-class CommandLineParser {
-public:
-    enum class ArgType {
-        Short, // Short option (eg "-a") or combined short option (eg "-af"). No more '-' is allowed
-        Long, // Long option (eg "--analysis").
-        Argument, // Argument or sub-command, not starting with "-" or "--" (eg "23" or "run")
-        Fail // Unsupported syntax (eg "-a-f")
-    };
-
-private:
-    std::vector<std::unique_ptr<CommandLineOptionBase>> _op;
-
-    /// Helper function to get arg type
-    static ArgType getArgType(char* argv) {
-        int i = 0;
-        ArgType t;
-        bool startHyphen = false;
-        bool startHyphen2 = false;
-        while(argv[i]) {
-            if(i == 0 && argv[i] == '-') startHyphen = true;
-            if(i == 1 && startHyphen && argv[i] == '-') startHyphen2 = true;
-            if(i > 1 && startHyphen && !startHyphen2 && argv[i] == '-') return ArgType::Fail;
-            ++i;
-        }
-        if(startHyphen2) return ArgType::Long;
-        if(startHyphen) return ArgType::Short;
-        return ArgType::Argument;
-    }
-
-public:
-    void parse(int argc, char** argv);
-};
 
 } // namespace medyan
 
