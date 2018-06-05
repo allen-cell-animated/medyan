ce5fce1dfc4da8425ea0da1467815facff74fe62
qin ni <qinni@qins-imac.local>
2018-06-05 15:59:16 -0400

    Bug fix based on commit 06b010e and part of 17eda20

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -578,6 +578,21 @@ void Controller::run() {
      cout << "Current simulation time = "<< tau() << endl;
     //restart phase ends
     }
+    
+    //perform first minimization
+#ifdef MECHANICS
+    _mController->run(false);
+    
+    //reupdate positions and neighbor lists
+    updatePositions();
+    updateNeighborLists();
+    
+#ifdef DYNAMICRATES
+    updateReactionRates();
+#endif
+    
+#endif
+    
 #ifdef CHEMISTRY
     tauLastSnapshot = tau();
     oldTau = 0;
@@ -609,6 +624,10 @@ void Controller::run() {
             if(tauLastMinimization >= _minimizationTime) {
                 _mController->run();
                 updatePositions();
+                
+#ifdef DYNAMICRATES
+                updateReactionRates();
+#endif
 
                 tauLastMinimization = 0.0;
             }
@@ -624,9 +643,6 @@ void Controller::run() {
             i++;
 #endif
 
-#ifdef DYNAMICRATES
-            updateReactionRates();
-#endif
             
 #ifdef CHEMISTRY
             // update neighbor lists
@@ -669,6 +685,10 @@ void Controller::run() {
                 _mController->run();
                 updatePositions();
                 
+#ifdef DYNAMICRATES
+                updateReactionRates();
+#endif
+                
                 stepsLastMinimization = 0;
             }
             
@@ -682,10 +702,7 @@ void Controller::run() {
             for(auto o: _outputs) o->print(i);
             i++;
 #endif
-            
-#ifdef DYNAMICRATES
-            updateReactionRates();
-#endif
+        
             
 #ifdef CHEMISTRY
             // update neighbor lists
