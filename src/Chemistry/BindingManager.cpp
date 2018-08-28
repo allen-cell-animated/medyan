3a75347e9d29f604f683d5e702ebafc80df30a18
Aravind Chandrasekaran <achansek@umd.edu>
2018-08-28 15:59:41 +0000

    BindingManager.cpp edited online with Bitbucket Removing unnecessary outputs.

--- src/Chemistry/BindingManager.cpp
+++ src/Chemistry/BindingManager.cpp
@@ -433,8 +433,7 @@ void LinkerBindingManager::removePossibleBindings(CCylinder* cc, short bindingSi
         
         int oldNOther = m->_bindingSpecies->getN();
         int newNOther = m->numBindingSites();
-        std::cout<<"removed affected mgr rxns "<<abs(oldN-newN)<<"(Old "<<oldN<<" NewN "
-                ""<<newN<<endl;
+
         m->updateBindingReaction(oldNOther, newNOther);
     }
 }
@@ -589,15 +588,13 @@ vector<tuple<CCylinder*, short>> LinkerBindingManager::chooseBindingSites() {
     assert((_possibleBindings.size() != 0)
 	    && "Major bug: Linker binding manager should not have zero binding \
             sites when called to choose a binding site.");
-    std::cout<<"Choose binding sites"<<endl;
+
     int randomIndex = Rand::randInteger(0, _possibleBindings.size() - 1);
 
     auto it = _possibleBindings.begin();
 
     advance(it, randomIndex);
 
-    auto xxx = _compartment->coordinates();
-    std::cout<<"Compartment coords "<<xxx[0]<<" "<<xxx[1]<<" "<<xxx[2]<<endl;
 #ifdef DEBUGCONSTANTSEED
     return (*it);
 #else
@@ -1052,13 +1049,11 @@ vector<tuple<CCylinder*, short>> MotorBindingManager::chooseBindingSites() {
     assert((_possibleBindings.size() != 0)
            && "Major bug: Motor binding manager should not have zero binding \
            sites when called to choose a binding site.");
-    std::cout<<"Choose binding sites"<<endl;
+           
     int randomIndex = Rand::randInteger(0, _possibleBindings.size() - 1);
     auto it = _possibleBindings.begin();
 
     advance(it, randomIndex);
-    auto xxx = _compartment->coordinates();
-    std::cout<<"Compartment coords "<<xxx[0]<<" "<<xxx[1]<<" "<<xxx[2]<<endl;
 #ifdef DEBUGCONSTANTSEED
     return (*it);
 #else
