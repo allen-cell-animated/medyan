114166cd2dbac226ece20826e5dab0ebf0c6b46a
Haoran Ni <drel.sohu.com@gmail.com>
2018-08-29 17:41:33 -0400

    Added log initialization and used Controller::initialize as example.

--- src/MEDYAN.cpp
+++ src/MEDYAN.cpp
@@ -71,6 +71,7 @@ The cell cytoskeleton plays a key role in human biology and disease, contributin
 
 #include "Controller.h"
 #include "SubSystem.h"
+#include "util/io/log.h"
 
 void printUsage() {
     cout << "Usage: MEDYAN -s systemFile -i inputDirectory -o outputDirectory" << endl;
@@ -125,7 +126,9 @@ int main(int argc, char **argv) {
         printUsage();
         exit(EXIT_FAILURE);
     }
-    
+
+    MEDYAN_LOG_DEFAULT_CONFIGURATION(outputDirectory + "/medyan.log");
+
     //initialize and run system
     c.initialize(inputFile, inputDirectory, outputDirectory);
     c.run();
