53b9030eba6c6b81593f503b97d66e4f46af28a1
cabb99 <carlos.bueno@rice.edu>
2019-02-11 12:13:59 -0600

    Adds more linker code to Binding Manager

--- src/Chemistry/BindingManager.cpp
+++ src/Chemistry/BindingManager.cpp
@@ -530,24 +530,68 @@ void CaMKIIBundlingManager::addPossibleBindings(CCylinder* cc, short bindingSite
 
 void CaMKIIBundlingManager::addPossibleBindings(CCylinder* cc) {
 
-
     for(auto bit = SysParams::Chemistry().bindingSites[_filamentType].begin();
              bit != SysParams::Chemistry().bindingSites[_filamentType].end(); bit++)
 
-        addPossibleBindings(cp, cc, *bit);
+        addPossibleBindings(cc, *bit);
 }
 
 void CaMKIIBundlingManager::removePossibleBindings(CCylinder* cc, short bindingSite) {
 
     if(cc->getType() != _filamentType) return;
 
-    //remove tuple which has this ccylinder
-    _possibleBindings.erase(cp);
+    //if we change other managers copy number
+    vector<LinkerBindingManager*> affectedManagers;
+
+    //remove all tuples which have this ccylinder as key
+    auto t = tuple<CCylinder*, short>(cc, bindingSite);
+    _possibleBindings.erase(t);
+
+    //remove all tuples which have this as value
+    for (auto it = _possibleBindings.begin(); it != _possibleBindings.end(); ) {
+
+        if (get<0>(it->second) == cc && get<1>(it->second) == bindingSite)
+            _possibleBindings.erase(it++);
+
+        else ++it;
+    }
 
     int oldN = _bindingSpecies->getN();
     int newN = numBindingSites();
 
     updateBindingReaction(oldN, newN);
+
+    //remove all neighbors which have this binding site pair
+    for (auto cn : _neighborLists[_nlIndex]->getNeighbors(cc->getCylinder())) {
+
+        if(cn->getType() != _filamentType) continue;
+
+        if(cn->getCompartment() != _compartment) {
+
+            auto m = (LinkerBindingManager*)cn->getCompartment()->
+                      getFilamentBindingManagers()[_mIndex].get();
+
+            if(find(affectedManagers.begin(), affectedManagers.end(), m) == affectedManagers.end())
+                affectedManagers.push_back(m);
+        }
+    }
+
+    //remove, update affected
+    for(auto m : affectedManagers) {
+
+        for (auto it = m->_possibleBindings.begin(); it != m->_possibleBindings.end(); ) {
+
+            if (get<0>(it->second) == cc && get<1>(it->second) == bindingSite)
+                m->_possibleBindings.erase(it++);
+
+            else ++it;
+        }
+
+        int oldNOther = m->_bindingSpecies->getN();
+        int newNOther = m->numBindingSites();
+
+        m->updateBindingReaction(oldNOther, newNOther);
+    }
 }
 
 
@@ -556,69 +600,123 @@ void CaMKIIBundlingManager::removePossibleBindings(CCylinder* cc) {
     for(auto bit = SysParams::Chemistry().bindingSites[_filamentType].begin();
              bit != SysParams::Chemistry().bindingSites[_filamentType].end(); bit++)
 
-        removePossibleBindings(cp, cc, *bit);
+        removePossibleBindings(cc, *bit);
 }
 
 
 void CaMKIIBundlingManager::updateAllPossibleBindings() {
 
-    //clear all
     _possibleBindings.clear();
 
-    for(auto &c : _compartment->getCylinders()) {
+    for(auto c : _compartment->getCylinders()) {
 
         if(c->getType() != _filamentType) continue;
 
         auto cc = c->getCCylinder();
 
-        //now re add valid binding sites
-        for(auto it = SysParams::Chemistry().bindingSites[_filamentType].begin();
-                 it != SysParams::Chemistry().bindingSites[_filamentType].end(); it++) {
+        for(auto it1 = SysParams::Chemistry().bindingSites[_filamentType].begin();
+                 it1 != SysParams::Chemistry().bindingSites[_filamentType].end(); it1++) {
 
-            bool inZone = true;
+            //now re add valid binding sites
+            if (areEqual(cc->getCMonomer(*it1)->speciesBound(
+                SysParams::Chemistry().linkerBoundIndex[_filamentType])->getN(), 1.0) ) {
 
-            if (areEqual(cc->getCMonomer(*it)->speciesBound(
-                SysParams::Chemistry().camkiierBoundIndex[_filamentType])->getN(), 1.0) && inZone) {
+                //loop through neighbors
+                //now re add valid based on CCNL
+                for (auto cn : _neighborLists[_nlIndex]->getNeighbors(cc->getCylinder())) {
 
-                auto t = tuple<CCylinder*, short>(cc, *it);
-                _possibleBindings.insert(t);
+                    if(cn->getParent() == c->getParent()) continue;
+                    if(cn->getType() != _filamentType) continue;
+
+                    auto ccn = cn->getCCylinder();
+
+                    for(auto it2 = SysParams::Chemistry().bindingSites[_filamentType].begin();
+                             it2 != SysParams::Chemistry().bindingSites[_filamentType].end(); it2++) {
+
+                        if (areEqual(ccn->getCMonomer(*it2)->speciesBound(
+                            SysParams::Chemistry().linkerBoundIndex[_filamentType])->getN(), 1.0)) {
+
+                            //check distances..
+                            auto mp1 = (float)*it1 / SysParams::Geometry().cylinderNumMon[_filamentType];
+                            auto mp2 = (float)*it2 / SysParams::Geometry().cylinderNumMon[_filamentType];
+
+                            auto x1 = c->getFirstBead()->coordinate;
+                            auto x2 = c->getSecondBead()->coordinate;
+                            auto x3 = cn->getFirstBead()->coordinate;
+                            auto x4 = cn->getSecondBead()->coordinate;
+
+                            auto m1 = midPointCoordinate(x1, x2, mp1);
+                            auto m2 = midPointCoordinate(x3, x4, mp2);
+
+                            double dist = twoPointDistance(m1,m2);
+
+                            if(dist > _rMax || dist < _rMin) continue;
+
+			    //auto bc1 = cc->getCylinder()->getBranchingCylinder(); //Carlos
+		            //auto bc2 = ccn->getCylinder()->getBranchingCylinder(); //Carlos
+			    auto bc3 = cc->getCylinder()->getMotherCylinder(); //Carlos
+		            auto bc4 = ccn->getCylinder()->getMotherCylinder(); //Carlos
+
+		    	    if(bc3 || bc4) {
+ 		            continue;} //Carlos
+
+
+                            auto t1 = tuple<CCylinder*, short>(cc, *it1);
+                            auto t2 = tuple<CCylinder*, short>(ccn, *it2);
+
+                            //add in correct order
+                            if(c->getID() > cn->getID())
+                                _possibleBindings.emplace(t1, t2);
+                        }
+                    }
+                }
             }
         }
     }
-
     int oldN = _bindingSpecies->getN();
     int newN = numBindingSites();
 
     updateBindingReaction(oldN, newN);
 }
 
+
 bool CaMKIIBundlingManager::isConsistent() {
 
     for (auto it = _possibleBindings.begin(); it != _possibleBindings.end(); it++) {
 
-        CCylinder* cc = get<0>(*it->first);
-        Cylinder* c   = cc->getCylinder();
+        CCylinder* cc1 = get<0>(it->first);
+        Cylinder*  c1  = cc1->getCylinder();
 
-        short bindingSite = get<1>(*it);
+        CCylinder* cc2 = get<0>(it->second);
+        Cylinder*  c2  = cc2->getCylinder();
+
+        short bindingSite1 = get<1>(it->first);
+        short bindingSite2 = get<1>(it->second);
 
         bool flag = true;
 
         //check site empty
-        if(!areEqual(cc->getCMonomer(bindingSite)->speciesBound(
-           SysParams::Chemistry().camkiierBoundIndex[_filamentType])->getN(), 1.0))
-            flag = false;
+                if(!areEqual(cc1->getCMonomer(bindingSite1)->speciesBound(
+                   SysParams::Chemistry().linkerBoundIndex[_filamentType])->getN(), 1.0) ||
 
-        if(!flag) {
-            cout << "Binding site in camkiiing manager is inconsistent. " << endl;
-            cout << "Binding site = " << bindingSite << endl;
+                   !areEqual(cc2->getCMonomer(bindingSite2)->speciesBound(
+                   SysParams::Chemistry().linkerBoundIndex[_filamentType])->getN(), 1.0))
 
-            cout << "Cylinder info ..." << endl;
-            c->printSelf();
+                    flag = false;
 
-            return false;
-        }
-    }
-    return true;
+                if(!flag) {
+                    cout << "Binding site in linker manager is inconsistent. " << endl;
+                    cout << "Binding site for cylinder 1 = " << bindingSite1 << endl;
+                    cout << "Binding site for cylinder 2 = " << bindingSite2 << endl;
+
+                    cout << "Cylinder info ..." << endl;
+                    c1->printSelf();
+                    c2->printSelf();
+
+                    return false;
+                }
+            }
+            return true;
 }
 //LINKER
 
