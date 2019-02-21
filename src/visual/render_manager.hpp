36f8cabd9de8f71eb032eff2562df7f7234cec39
drelatbitbucket <drel.sohu.com@gmail.com>
2019-02-21 13:01:06 -0500

    chore: random framework

---
+++ src/visual/render_manager.hpp
@@ -0,0 +1,20 @@
+#ifndef MEDYAN_VISUAL_RENDER_MANAGER_HPP
+#define MEDYAN_VISUAL_RENDER_MANAGER_HPP
+
+#include "visual/common.hpp"
+
+namespace visual {
+
+// The render manager is used to manage initiating/destroying the rendering
+// context and threads.
+// It should be able to store/update the settings for rendering and
+// update it.
+// It should be able to access system data (as well as settings) and update
+// the rendering result.
+class RenderManager {
+
+};
+
+} // namespace visual
+
+#endif
