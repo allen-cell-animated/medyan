d66b59a904555be053fe40bc5e3026c5c0fbda2b
drelatgithub <drel.sohu.com@gmail.com>
2019-04-09 01:14:09 -0400

    feat: starting to index the cuboid tetrahedra

--- src/Structure/SurfaceMesh/SurfaceMeshGenerator.hpp
+++ src/Structure/SurfaceMesh/SurfaceMeshGenerator.hpp
@@ -22,7 +22,7 @@ public:
 
     struct TetraFaceData {
         std::size_t firstHalfEdgeIdxInMesh;
-        bool hasHalfEdge;
+        bool hasHalfEdge = false;
     };
 
     MarchingTetrahedraGenerator(
@@ -65,6 +65,7 @@ public:
         // one neg: gen triangle pointing neg
         // 2 pos 2 neg: gen 2 triangles
         get_vtx_idx from (nx ny nz and tetIdx);
+        get face index from (nx ny nz and tetIdx);
 
         small_size_t cond = 0; // 0b x x x x <-- each bit is 1 if sign is pos, 0 if sign is neg
         update cond();
@@ -81,28 +82,61 @@ public:
     }
 
     // Helper function: sewing a new triangle to the surface
+    template< typename Mesh >
     void sewTriangle(
+        Mesh& mesh,
         std::size_t nx, std::size_t ny, std::size_t nz, small_size_t tetIdx,
         small_size_t e0Idx, small_size_t e1Idx, small_size_t e2Idx
     ) const {
+        using std::size_t;
+
         get face idx from nx ny nz and tetIdx, e Indices
 
+        typename Mesh::NewTrianglePatch{} (
+            mesh,
+            {v??, v??, v??},
+            {_faceData[??].hasHalfEdge, _faceData[??].hasHalfEdge, _faceData[??].hasHalfEdge},
+            {_faceData[??].firstHalfEdgeIdxInMesh, _faceData[??].firstHalfEdgeIdxInMesh, _faceData[??].firstHalfEdgeIdxInMesh}
+        );
         tell "mesh" to add a new triangle: (v0, v1, v2, he0op, he0opIdx, he1op, he1opIdx, he2op, he2opIdx)
-        update face data =
-        for i = 0:3 get face data:
-            if face has an halfedge (ref target vertex, and edge) -> create opposite
-            else -> create a single halfedge (ref target vertex) and an edge (ref complete); -> update halfedge (ref edge)
-        create triangle (ref complete)
-        update halfedges (ref triangle, next, prev)
-
-        update ref for vertices
     }
 
 private:
     // Constants
+    //-------------------------------------------------------------------------
+    // The vertices of the tetrahedra are v0, v1, v2, v3, which must satisfy
+    //     r01 x r12 . r23 > 0 (specifically, the cuboid volume)
+    //
+    // The edges in a tetrahedra are in the following order:
+    //     01, 02, 03, 12, 13, 23
+    //
+    // Tetrahedra in a cuboid is ordered in the following order (labeled using r's):
+    //     ijk, (i+j)(-i)(k+i), jki, (j+k)(-j)(i+j), kij, (k+i)(-k)(j+k)
     static constexpr small_size_t _numTetrahedraPerCuboid = 6;
     static constexpr small_size_t _numTetraFacesPerCuboid = 12; // 6 on 3 faces and 6 inside the cuboid
-    static constexpr small_size_t _numTetraEdgesPerCuboid = 7; // 001 010 100, 110 101 011, 111
+    static constexpr small_size_t _numTetraEdgesPerCuboid = 7; // = 0bxyz - 1
+
+    // Local vertex index [local tetra idx (6)][vertex idx (4)]
+    static constexpr small_size_t _tetraVertexLocalIndex[6][4] {
+        0b000, 0b100, 0b110, 0b111,
+        0b000, 0b110, 0b010, 0b111,
+        0b000, 0b010, 0b011, 0b111,
+        0b000, 0b011, 0b001, 0b111,
+        0b000, 0b001, 0b101, 0b111,
+        0b000, 0b101, 0b100, 0b111
+    };
+    // Local edge index [local tetra idx (6)][edge idx (6)]
+    // Value:  0b  xyz                  d                      xyz
+    //             ^ starting position  ^ same(0)/diff(1) dir  ^ edge direction (positive)
+    // Real index in cuboid = 0bxyz(direction) - 1
+    static constexpr small_size_t _tetraEdgeLocalIndex[6][6] {
+        0b0000100, 0b0000110, 0b0000111, 0b1000010, 0b1000011, 0b1100001,
+        0b0000110, 0b0000010, 0b0000111, 0b1101001, 0b1100001, 0b0100101,
+        // TODO
+    };
+    // Local face index [local tetra idx (6)][face idx (4)]
+    // Value:    0b    xyz
+    //                 ^ cuboid position    ^ face ????
 
     // Parameters
     Float                           _minPositionShift = 1e-2; // The position will have a minimal shift from either end
@@ -150,6 +184,14 @@ private:
         }
     }
 };
+
+// static variable definition (declaration in C++17, and should be removed)
+template< typename Float >
+constexpr typename MarchingTetrahedraGenerator<Float>::small_size_t
+MarchingTetrahedraGenerator<Float>::_tetraVertexLocalIndex[6][4];
+template< typename Float >
+constexpr typename MarchingTetrahedraGenerator<Float>::small_size_t
+MarchingTetrahedraGenerator<Float>::_tetraEdgeLocalIndex[6][6];
 
 } // namespace mesh_gen
 
