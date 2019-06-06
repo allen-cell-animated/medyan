963d8a6f605e93586734d5f217a313dd9f8bdbe2
drelatbitbucket <drel.sohu.com@gmail.com>
2019-06-06 16:39:18 -0400

    chore: move ctor/dtor def of visual element

--- src/Visual/VisualElement.hpp
+++ src/Visual/VisualElement.hpp
@@ -38,35 +38,16 @@ struct GlState {
 
     // Set by init and helper
     GLenum eleMode = GL_TRIANGLES;
-};
-
-// Each VisualElement consists of the following parts:
-//   - A mutex that locks all the members
-//   - All the related settings
-//   - OpenGL compatible data structure as output
-// Note:
-//   - Due to constructor of the visual element, the object must be created
-//     within an OpenGL context.
-struct VisualElement {
-    std::mutex me;
-
-    // settings
-    //-------------------------------------------------------------------------
-    Profile profile;
-
-    // OpenGL data
-    //-------------------------------------------------------------------------
-    GlState state;
 
     // ctor and dtor
-    VisualElement() {
-        glGenBuffers(1, &state.vbo);
-        glGenBuffers(1, &state.ebo);
-        glGenVertexArrays(1, &state.vao);
+    GlState() {
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+        glGenVertexArrays(1, &vao);
 
-        glBindVertexArray(state.vao);
-        glBindBuffer(GL_ARRAY_BUFFER, state.vbo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, state.ebo);
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
         // Vertex attribute
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -78,11 +59,31 @@ struct VisualElement {
         glBindVertexArray(0);
     }
 
-    ~VisualElement() {
-        glDeleteVertexArrays(1, &state.vao);
-        glDeleteBuffers(1, &state.vbo);
-        glDeleteBuffers(1, &state.ebo);
+    ~GlState() {
+        glDeleteVertexArrays(1, &vao);
+        glDeleteBuffers(1, &vbo);
+        glDeleteBuffers(1, &ebo);
     }
+
+};
+
+// Each VisualElement consists of the following parts:
+//   - A mutex that locks all the members
+//   - All the related settings
+//   - OpenGL compatible data structure as output
+// Note:
+//   - Due to constructor of the GlState element, the object must be created
+//     within an OpenGL context.
+struct VisualElement {
+    std::mutex me;
+
+    // settings
+    //-------------------------------------------------------------------------
+    Profile profile;
+
+    // OpenGL data
+    //-------------------------------------------------------------------------
+    GlState state;
 
 };
 
