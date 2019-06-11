fcbc9211d16f6f288b579c7fc99041b7cb1fc72d
drelatbitbucket <drel.sohu.com@gmail.com>
2019-06-11 17:03:32 -0400

    feat: reimpl jet interpolation

--- src/Util/Math/ColorMap.hpp
+++ src/Util/Math/ColorMap.hpp
@@ -1,11 +1,12 @@
 #ifndef MEDYAN_Util_Math_ColorMap_Hpp
 #define MEDYAN_Util_Math_ColorMap_Hpp
 
+#include <array>
 #include <cmath> // abs
 
 #include "Util/Math/Vec.hpp"
 
-// A colormap is a function, which transforms a real number (0.0 - 1.0) to an
+// A colormap is a function, which transforms a real number in a range to an
 // RGB value
 namespace colormap {
 
@@ -19,19 +20,43 @@ namespace utility {
     template< typename Float >
     constexpr Float clamp01(Float v) { return clamp(v, (Float)0.0, (Float)1.0); }
 
+    template< typename FloatVal, size_t colorDim, typename FloatColor, size_t listSize >
+    constexpr mathfunc::Vec< colorDim, FloatColor >
+    interpolate(FloatVal v, std::array< mathfunc::Vec< colorDim, FloatColor >, listSize > interpList) {
+        static_assert(listSize > 1, "Must have at least 2 elements for interpolation");
+        constexpr FloatVal interval = static_cast< FloatVal >(1.0) / (listSize - 1);
+        constexpr FloatVal normalizedPos = v / interval;
+        constexpr size_t intervalIndex = static_cast< size_t >(normalizedPos);
+        if(intervalIndex >= listSize - 1) return interpList[listSize - 1];
+
+        return interpList[intervalIndex    ] * (intervalIndex + 1 - normalizedPos)
+            +  interpList[intervalIndex + 1] * (normalizedPos - intervalIndex);
+    }
+
 } // namespace utility
 
-template< typename FloatOut, typename FloatIn >
-inline
-mathfunc::Vec< 3, FloatOut >
-jet(FloatIn v) {
-    constexpr auto interpolation = [](FloatIn v) { return colormap::utility::clamp01((FloatIn)1.5 - std::abs(2 * v)); };
-    return mathfunc::Vec< 3, FloatOut > {
-        static_cast< FloatOut >(interpolation(v - 0.5)),
-        static_cast< FloatOut >(interpolation(v      )),
-        static_cast< FloatOut >(interpolation(v + 0.5))
-    };
-}
+template< typename FloatColor >
+struct Jet {
+    static constexpr std::array< mathfunc::Vec< 3, FloatColor >, 9 > interpList = {{
+        { 0.0, 0.0, 0.5 },
+        { 0.0, 0.0, 1.0 },
+        { 0.0, 0.5, 1.0 },
+        { 0.0, 1.0, 1.0 },
+        { 0.5, 1.0, 0.5 },
+        { 1.0, 1.0, 0.0 },
+        { 1.0, 0.5, 0.0 },
+        { 1.0, 0.0, 0.0 },
+        { 0.5, 0.0, 0.0 }
+    }};
+
+    template< typename FloatIn >
+    constexpr auto operator()(FloatIn v) const {
+        return utility::interpolate(v, interpList);
+    }
+};
+template< typename FloatColor >
+constexpr std::array< mathfunc::Vec< 3, FloatColor >, 9 >
+Jet< FloatColor >::interpList; // remove in C++17
 
 } // namespace colormap
 
