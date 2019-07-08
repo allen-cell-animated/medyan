78d8ae996b051ba95659488792dc55107eddd644
drelatbitbucket <drel.sohu.com@gmail.com>
2019-07-08 19:29:21 -0400

    feat: (incomplete) start to add br-dih test

---
+++ src/TESTS/Mechanics/ForceField/Branching/TestBranchingDihedralQuadratic.cpp
@@ -0,0 +1,7 @@
+#include "catch2/catch.hpp"
+
+#include "Mechanics/ForceField/Branching/BranchingDihedralQuadratic.hpp"
+
+TEST_CASE("Force field: Branching Dihedral Quadratic", "[ForceField]") {
+    // TODO
+}
---
+++ src/TESTS/Mechanics/ForceField/TestFFCommon.hpp
@@ -0,0 +1,6 @@
+#ifndef MEDYAN_TESTS_Mechanics_ForceField_TestFFCommon_Hpp
+#define MEDYAN_TESTS_Mechancis_ForceField_TestFFCommon_Hpp
+
+// Provides common methods for force field testing purposes
+
+#endif
