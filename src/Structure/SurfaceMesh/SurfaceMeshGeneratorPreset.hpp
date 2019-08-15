75ca8e20b243298c77978dfc09b0295a735bf870
drelatbitbucket <drel.sohu.com@gmail.com>
2019-08-15 15:11:35 -0400

    feat: add mesh plane initializer

--- src/Structure/SurfaceMesh/SurfaceMeshGeneratorPreset.hpp
+++ src/Structure/SurfaceMesh/SurfaceMeshGeneratorPreset.hpp
@@ -46,6 +46,31 @@ generateEllipsoid(const mathfunc::Vec< 3, Float >& center, const mathfunc::Vec< 
 }
 
 template< typename Float = double >
+inline auto generatePlane(
+    const mathfunc::Vec< 3, Float >& pointOnPlane, const mathfunc::Vec< 3, Float >& normal,
+    const mathfunc::Vec< 3, Float >& boundingBoxOrigin, const mathfunc::Vec< 3, Float >& boundingBoxSize
+) {
+    using namespace mathfunc;
+
+    constexpr Float cubeSize = 40.0;
+
+    const std::array< std::size_t, 3 > numCubes {
+        size_t(boundingBoxSize[0] / cubeSize),
+        size_t(boundingBoxSize[1] / cubeSize),
+        size_t(boundingBoxSize[2] / cubeSize)
+    };
+    // Overall size might be smaller than specified bounding box size
+
+    const auto un = normalizedVector(normal);
+
+    return MarchingTetrahedraGenerator< Float >(cubeSize, boundingBoxOrigin, numCubes)(
+        [&](const Vec< 3, Float >& v) {
+            return dot(v - pointOnPlane, un);
+        }
+    );
+}
+
+template< typename Float = double >
 inline typename MarchingTetrahedraGenerator<Float>::Result
 generateMeshViaParams(const std::vector< std::string >& param) {
     if(param.empty()) {
@@ -66,6 +91,26 @@ generateMeshViaParams(const std::vector< std::string >& param) {
             iss >> value[i];
         }
         return generateEllipsoid< Float >({value[0], value[1], value[2]}, {value[3], value[4], value[5]});
+    }
+
+    if(param[0] == "PLANE") {
+        if(param.size() < 13) {
+            LOG(ERROR) << "Mesh plane initializer has 12 parameters";
+            LOG(INFO) << "Mesh plane initializer: PLANE center_xyz... normal_xyz... box_origin_xyz... box_size_abc...";
+            throw std::runtime_error("Insufficient parameters in mesh plane initializer");
+        }
+
+        Float value[12];
+        for(size_t i = 0; i < 12; ++i) {
+            std::istringstream iss(param[i+1]);
+            iss >> value[i];
+        }
+        return generatePlane< Float >(
+            {value[0], value[1], value[2]},
+            {value[3], value[4], value[5]},
+            {value[6], value[7], value[8]},
+            {value[9], value[10], value[11]}
+        );
     }
 
     LOG(ERROR) << "Unrecognized mesh initializer";
