4a2f61050fb352792a9c9bd2ddf310b01df2f4a1
aravind <achansek@umd.edu>
2019-10-30 17:33:19 -0400

    BUG FIXES: NONE CHANGES a. Done with simulation will now be displayed at the end of every simulation!

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1721,7 +1721,7 @@ void Controller::run() {
         cout << "Dynamic neighbor " << SubSystem::timedneighbor << endl;
         cout << "Neighbor " << SubSystem::timeneighbor << endl;
         cout << "Trackable " << SubSystem::timetrackable << endl;
-        cout << "Done with simulation!" << endl;
+
         cout << "-------------" << endl;
         cout << "Filament extendPlusEnd 1 " << Filament::FilextendPlusendtimer1 << endl;
         cout << "Filament extendPlusEnd 2 " << Filament::FilextendPlusendtimer2 << endl;
@@ -1745,6 +1745,7 @@ void Controller::run() {
              << CUDAcommon::ppendtime.rxntempate4 << endl;
     }
 	#endif
+    cout << "Done with simulation!" << endl;
 #ifdef CUDAACCL
     cudaDeviceReset();
 #endif
