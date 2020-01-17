2cc60d75af269cb12e193e494826be7beb89ce85
drelatbitbucket <drel.sohu.com@gmail.com>
2020-01-16 21:52:22 -0500

    fix: medyan dt2 build

--- build-medyan-dt2.sh
+++ build-medyan-dt2.sh
@@ -6,11 +6,11 @@ medyan_root_dir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 
 # Set up variables
 export MEDYAN_BOOST_INSTALL_MODE="manual"
-export MEDYAN_BOOST_INCLUDE_DIR="$(BOOST_INCLUDE)"
-export MEDYAN_BOOST_LIBRARY_DIR="$(BOOST_LIB)"
+export MEDYAN_BOOST_INCLUDE_DIR="$BOOST_INCLUDE"
+export MEDYAN_BOOST_LIBRARY_DIR="$BOOST_LIB"
 export MEDYAN_BOOST_LIBRARIES="boost_system"
-export MEDYAN_ADDITIONAL_LINK_DIRS="$(GCC_ROOTDIR)/lib64"
-export MEDYAN_RPATH="$(GCC_ROOTDIR)/lib64"
+export MEDYAN_ADDITIONAL_LINK_DIRS="$GCC_ROOTDIR/lib64"
+export MEDYAN_RPATH="$GCC_ROOTDIR/lib64"
 
 # Run the script
 $medyan_root_dir/build-medyan.sh
