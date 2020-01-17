af8ae8eb8020ee6c761b8748ea682e3b647a0e0b
drelatbitbucket <drel.sohu.com@gmail.com>
2020-01-17 00:17:54 -0500

    fix: git version check

--- build-medyan-dt2.sh
+++ build-medyan-dt2.sh
@@ -5,14 +5,17 @@
 medyan_root_dir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 
 # Install git for vcpkg (requires >= 2.7.0)
-if [ "$(git version | cut -d\".\" -f2)" -lt 7 ]; then
+export PATH="~/bin:$PATH"
+if [ "$(git version | cut -d"." -f2)" -lt 7 ]; then
+    echo "git version is too old. Building a new git version..."
     (
         cd $medyan_root_dir/scripts/.build &&
         wget https://github.com/git/git/archive/v2.25.0.zip -O git.zip &&
         unzip -qq git.zip &&
+        cd git-2.25.0 &&
         make -j10 &&
         make install
-    ) && export PATH="~/bin:$PATH"
+    )
 fi
 
 # Set up variables
